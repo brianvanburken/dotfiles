0679a7b5f6a5362863c44132a95490db04ed67d6
Brian van Burken <brianvanburken@users.noreply.github.com>
2015-03-05 12:01:32 +0100

    Added .bash_login.

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,9 +1,9 @@
 #!/bin/bashln -s ~/Dropbox/Dotfiles/.bash_aliases
 sudo -v
 
-files=(.agignore .bash_aliases .bash_profile .bashrc .bundle .csscomb.json .gemrc
-       .gitconfig .gitignore_global .powconfig .profile .railsrc .slate .vim
-       .vimrc .zlogin .zshrc)
+files=(.agignore .bash_aliases .bash_login .bash_profile .bashrc .bundle
+        .csscomb.json .gemrc .gitconfig .gitignore_global .powconfig .profile
+        .railsrc .slate .vim .vimrc .zlogin .zshrc)
 dir="$PWD/"
 
 for i in ${files[@]}; do
