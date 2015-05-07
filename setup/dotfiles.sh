c880c3d4408790b6dd75b557a6d153940d9bc05d
Brian van Burken <brian@creativeview.nl>
2015-05-07 10:55:35 +0200

    Added hushlogin for hiding banner and speeding up terminal.

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -2,8 +2,8 @@
 sudo -v
 
 files=(.agignore .bash_aliases .bash_login .bash_profile .bashrc .bundle
-        .csscomb.json .gemrc .gitconfig .gitignore_global .powconfig .profile
-        .slate .tmux.conf .vim .vimrc)
+        .csscomb.json .gemrc .gitconfig .gitignore_global .hushlogin .powconfig
+        .profile .slate .tmux.conf .vim .vimrc)
 dir="$PWD/"
 
 for i in ${files[@]}; do
