39d6b4012272ef6a83bb738aa33d1acf413c1db7
Brian van Burken <brianvanburken@users.noreply.github.com>
2015-08-14 19:35:21 +0200

    Install script for software added.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
-# Ask for the administrator password upfront
+#!/bin/sh
 sudo -v
 
 sh setup/dotfile.sh
 sh setup/osx.sh
 sh setup/packages.sh
+sh setup/software.sh
 
 echo "Done. Note that some of these changes require a logout/restart to take effect."
---
+++ setup/software.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+echo Install all AppStore Apps at first!
+read -p "Press any key to continue... " -n1 -s
+echo  '\n'
+echo Installing homebrew caskroom...
+brew tap phinze/cask
+brew install brew-cask
+
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+
+read -p "Do you want to install Afred? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  brew cask install alfred
+fi
+
+read -p "Do you want to install Firefox? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  brew cask install firefox
+fi
+
+read -p "Do you want to install Opera? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  brew cask install opera
+fi
+
+read -p "Do you want to install Google Chrome? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  brew cask install google-chrome
+fi
+
+read -p "Do you want to install f.lux? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  brew cask install flux
+fi
+
+read -p "Do you want to install Slate? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  brew cask install mattr-slate
+fi
+
+read -p "Do you want to install Cookie? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  brew cask install cookie
+fi
