a0807a3dff7b89a774aff6434b65bccd4f1d801d
Brian van Burken <brianvanburken@outlook.com>
2016-09-28 19:32:25 +0200

    Install homebrew as normal.

--- install.sh
+++ install.sh
@@ -46,11 +46,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 if ! command -v brew >/dev/null; then
   fancy_echo "Installing Brew"
-  source .profile
-  brew_path=$HOME/.homebrew
-  mkdir -p ${brew_path}
-  chown -R $(whoami) ${brew_path}
-  curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C ${brew_path}
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
   fancy_echo "Updating Brew ..."
   brew update >> out.log
