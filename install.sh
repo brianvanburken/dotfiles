ade0406a2670bf165009405e33bc5b6ca53bf9af
Brian van Burken <brianvanburken@users.noreply.github.com>
2020-08-03 10:34:23 +0200

    Clean up install script

--- install.sh
+++ install.sh
@@ -14,7 +14,6 @@ fancy_echo "Creating local files"
 touch ~/.gitconfig.local
 touch ~/.hushlogin
 
-fancy_echo "Cloning git repository for dotfiles"
 DOT_DIR=~/Developer/personal/dotfiles
 if [ ! -d $DOT_DIR ]; then
     fancy_echo "Cloning dotfiles"
@@ -31,25 +30,21 @@ which -s brew
 if [[ $? != 0 ]] ; then
 	fancy_echo "Installing Homebrew"
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    brew doctor
 	fancy_echo "Homebrew is installed"
+    fancy_echo "Installing Homebrew packages"
+    brew bundle
 else
 	fancy_echo "Homebrew already installed"
+    brew update
 fi
 
-fancy_echo "Installing Homebrew packages"
-brew doctor
-brew update
-brew bundle
-
-PLUG_DIR=$XDG_DATA_HOME:-$HOME/.local/share
+PLUG_DIR=$XDG_DATA_HOME:=$HOME/.local/share
 if [ ! -d $PLUG_DIR ]; then
     fancy_echo "Installing vim plugins"
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
     nvim +PlugInstall +qall
-else
-    fancy_echo "Updating vim plugins"
-    nvim +PlugUpdate +qall
 fi
 
 fancy_echo "Linking dotfiles in $HOME to $DOT_DIR"
@@ -73,6 +68,7 @@ for x in "${files[@]}"; do
 	ln -s $DOT_DIR/$x $HOME/.$x
 done
 
+rm $HOME/.agignore
 ln -s $DOT_DIR/config/ag/ignore $HOME/.agignore
 
 fancy_echo "Adding asdf plugins"
