884d61ba5fba9a2cb6439c270da45c6eb8103bb2
Brian van Burken <brianvanburken@users.noreply.github.com>
2021-08-03 09:50:33 +0200

    Add vim guttentags and format NeoVim init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,52 +1,81 @@
-vim.api.nvim_command('packadd packer.nvim')
-require('packer').startup(function(use)
-  use { 'wbthomason/packer.nvim', opt = true }
-  use { 'junegunn/fzf.vim', cmd = {'Ag', 'Files'}, requires = { '/usr/local/opt/fzf' } }
-  use { 'editorconfig/editorconfig-vim' }
-  use { 'luxed/ayu-vim', config = 'vim.cmd [[colorscheme ayu]]' }
-  use { 'neoclide/coc.nvim', branch = 'release', ft = { 'html', 'scss', 'typescript', 'lua', 'json' } }
-  use { 'tpope/vim-commentary' }
-  use { 'tpope/vim-surround' }
-end)
+-- Disable default plugins
+vim.g.loaded_gzip = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_matchparen = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_logiPat = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
+vim.g.loaded_netrwFileHandlers = 1
 
-vim.o.clipboard = 'unnamed' -- Share Clipboard with OS
+-- Disable some providers
+vim.g.loaded_node_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_python_provider = 0
+
+vim.o.clipboard = "unnamed" -- Share Clipboard with OS
 vim.o.cmdheight = 2
 vim.o.hidden = true
 vim.o.lazyredraw = true
-vim.o.shortmess = vim.o.shortmess..'aoOstTWAIcqFS' -- Shorten all messages
-vim.o.signcolumn = 'yes'
-vim.o.statusline = '%t%m%r%=%c:%l'
+vim.o.shortmess = vim.o.shortmess .. "aoOstTWAIcqFS" -- Shorten all messages
+vim.o.signcolumn = "yes"
+vim.o.statusline = "%t%m%r%=%c:%l"
 vim.o.termguicolors = true -- enable true colors support
 vim.o.timeoutlen = 400 -- 400 ms wait to sequence complete
 vim.wo.wrap = false
 
-vim.api.nvim_set_keymap('n', '<C-a>', ':Rg!<CR>', {})
-vim.api.nvim_set_keymap('n', '<C-p>', ':Files!<CR>', {})
+-- Packer
+vim.api.nvim_command("packadd packer.nvim")
+require("packer").startup(
+    function(use)
+        use {"editorconfig/editorconfig-vim"}
+        use {"junegunn/fzf.vim", cmd = {"Ag", "Files", "Tags"}, requires = {"/usr/local/opt/fzf"}}
+        use {"ludovicchabant/vim-gutentags"}
+        use {"luxed/ayu-vim", config = "vim.cmd [[colorscheme ayu]]"}
+        use {"neoclide/coc.nvim", branch = "release", ft = {"html", "scss", "typescript", "lua", "json"}}
+        use {"tpope/vim-commentary"}
+        use {"tpope/vim-surround"}
+        use {"wbthomason/packer.nvim", opt = true}
+    end
+)
 
--- CoC config
+-- Fzf
+vim.api.nvim_set_keymap("n", "<C-a>", ":Rg!<CR>", {})
+vim.api.nvim_set_keymap("n", "<C-p>", ":Files!<CR>", {})
+vim.api.nvim_set_keymap("n", "<C-t>", ":Tags!<CR>", {})
+
+-- CoC
 -- Use `[g` and `]g` to navigate diagnostics
 -- Use `:CocDiagnostics` to get all diagnostics of current buffer in location list.
-vim.api.nvim_set_keymap('n', '[g', '<Plug>(coc-diagnostic-prev)', { silent = true })
-vim.api.nvim_set_keymap('n', ']g', '<Plug>(coc-diagnostic-next)', { silent = true })
+vim.api.nvim_set_keymap("n", "[g", "<Plug>(coc-diagnostic-prev)", {silent = true})
+vim.api.nvim_set_keymap("n", "]g", "<Plug>(coc-diagnostic-next)", {silent = true})
 
 -- GoTo code navigation.
-vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', { silent = true })
-vim.api.nvim_set_keymap('n', 'gy', '<Plug>(coc-type-definition)', { silent = true })
-vim.api.nvim_set_keymap('n', 'gi', '<Plug>(coc-implementation)', { silent = true })
-vim.api.nvim_set_keymap('n', 'gr', '<Plug>(coc-references)', { silent = true })
+vim.api.nvim_set_keymap("n", "gd", "<Plug>(coc-definition)", {silent = true})
+vim.api.nvim_set_keymap("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
+vim.api.nvim_set_keymap("n", "gi", "<Plug>(coc-implementation)", {silent = true})
+vim.api.nvim_set_keymap("n", "gr", "<Plug>(coc-references)", {silent = true})
 
 -- Use K to show documentation in preview window.
 -- Source https://old.reddit.com/r/neovim/comments/jqdmuo/calling_plugin_functions_from_lua/
-vim.api.nvim_set_keymap('n', 'K', ':lua ShowDocumentation()<CR>', { silent = true })
+vim.api.nvim_set_keymap("n", "K", ":lua ShowDocumentation()<CR>", {silent = true})
 function ShowDocumentation()
-  local filetype = vim.bo.filetype
-  if filetype == "vim" or filetype == "help" then
-    vim.api.nvim_command("h " .. vim.fn.expand("<cword>"))
-  elseif vim.call("coc#rpc#ready") then
-    vim.fn.CocActionAsync("doHover")
-  else
-    vim.api.nvim_command(
-      "!" .. vim.bo.keywordprg .. " " .. vim.fn.expand("<cword>")
-    )
-  end
+    local filetype = vim.bo.filetype
+    if filetype == "vim" or filetype == "help" then
+        vim.api.nvim_command("h " .. vim.fn.expand("<cword>"))
+    elseif vim.call("coc#rpc#ready") then
+        vim.fn.CocActionAsync("doHover")
+    else
+        vim.api.nvim_command("!" .. vim.bo.keywordprg .. " " .. vim.fn.expand("<cword>"))
+    end
 end
