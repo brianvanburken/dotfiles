69b00a72a9cfc194c3a1f03fc65d3d20c57fed41
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-07-05 09:28:02 +0200

    feat: add extra treesitter configuration

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -111,7 +111,19 @@ require("packer").startup(
                         highlight = {
                             enable = true,
                             use_languagetree = true
-                        }
+                        },
+                        indent = {
+                            enable = true
+                        },
+                        incremental_selection = {
+                            enable = true,
+                            keymaps = {
+                                init_selection = "gnn",
+                                node_incremental = "grn",
+                                scope_incremental = "grc",
+                                node_decremental = "grm",
+                            },
+                        },
                     }
                 )
 
