dcd323d6f645b92926c1e2a813e4174af9be46b4
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-07-13 09:21:43 +0200

    feat: switch find in files shortcut CTRL-a from CTRL-f in NeoVim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -28,7 +28,7 @@ require("packer").startup(
                 vim.env.FZF_DEFAULT_OPTS = "--ansi"
 
                 vim.api.nvim_set_keymap("n", "<C-p>", ":Files!<CR>", {})
-                vim.api.nvim_set_keymap("n", "<C-f>", ":Rg!<CR>", {})
+                vim.api.nvim_set_keymap("n", "<C-a>", ":Rg!<CR>", {})
             end
         }
         use {
