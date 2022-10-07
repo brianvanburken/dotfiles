e31ea626152d46b9006e143333ecead906a7a73b
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-10-07 09:01:54 +0200

    feat: disable unused neovim providers

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,7 @@
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_python_provider = 0
+
 vim.o.autoread = true -- Automatically reload files changed outside of vim
 vim.o.clipboard = "unnamedplus"
 vim.o.cmdheight = 2
