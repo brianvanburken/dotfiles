57cacae78e93c35c866aba382784c67a09e1bdbb
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-10-27 15:42:14 +0200

    feat: use hard contrast with gruvbox theme in NeoVim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -43,6 +43,9 @@ require("packer").startup(
         use {
             "ellisonleao/gruvbox.nvim",
             config = function()
+                require("gruvbox").setup({
+                    contrast = "hard"
+                })
                 vim.cmd("colorscheme gruvbox")
                 vim.cmd("highlight Normal ctermbg=none guibg=none")
                 vim.cmd("highlight NonText ctermbg=none guibg=none")
