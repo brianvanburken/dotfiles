4df5ff7101601a6ac301fc90ee297c55f12a80ca
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-12-16 18:45:53 +0100

    feat: Add C-t for tag searches in FZF

--- config/nvim/lua/fzf.lua
+++ config/nvim/lua/fzf.lua
@@ -3,4 +3,5 @@ vim.env.FZF_ALT_C_COMMAND = vim.env.FZF_DEFAULT_COMMAND
 vim.env.FZF_DEFAULT_OPTS = "--ansi"
 
 vim.api.nvim_set_keymap("n", "<C-p>", ":Files!<CR>", {})
+vim.api.nvim_set_keymap("n", "<C-t>", ":Tags!<CR>", {})
 vim.api.nvim_set_keymap("n", "<C-a>", ":Rg!<CR>", {})
