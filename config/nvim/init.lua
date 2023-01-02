6014978590452afde69e83ad8d014a68e012c41e
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-01-02 14:46:53 +0100

    chore: format lua code with stylua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -37,127 +37,122 @@ vim.keymap.set("x", "<leader>p", '"_d')
 -- Setup Lazy.nvim
 vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
 
-require("lazy").setup(
+require("lazy").setup({
     {
-        {
-            "ibhagwan/fzf-lua",
-            keys = {
-                {"<C-p>", "<cmd>lua require('fzf-lua').files()<CR>"},
-                {"<C-t>", "<cmd>lua require('fzf-lua').tags()<CR>"},
-                {"<C-a>", "<cmd>lua require('fzf-lua').live_grep()<CR>"},
-                {"<C-i>", "<cmd>lua require('fzf-lua').builtin()<CR>"},
-                {"<leader>c", "<cmd>lua require('fzf-lua').commands()<CR>"}
+        "ibhagwan/fzf-lua",
+        keys = {
+            { "<C-p>", "<cmd>lua require('fzf-lua').files()<CR>" },
+            { "<C-t>", "<cmd>lua require('fzf-lua').tags()<CR>" },
+            { "<C-a>", "<cmd>lua require('fzf-lua').live_grep()<CR>" },
+            { "<C-i>", "<cmd>lua require('fzf-lua').builtin()<CR>" },
+            { "<leader>c", "<cmd>lua require('fzf-lua').commands()<CR>" },
+        },
+        config = {
+            winopts = {
+                fullscreen = true,
             },
-            config = {
-                winopts = {
-                    fullscreen = true
-                },
-                fzf_opts = {
-                    ["--layout"] = "default"
-                }
-            }
+            fzf_opts = {
+                ["--layout"] = "default",
+            },
         },
-        {
-            "ellisonleao/gruvbox.nvim",
-            init = function()
-                vim.cmd("colorscheme gruvbox")
-            end,
-            config = {
-                contrast = "hard"
-            }
-        },
-        {
-            "neoclide/coc.nvim",
-            build = ":CocUpdate",
-            branch = "release",
-            event = "BufReadPost",
-            config = function()
-                require("coc")
-            end
-        },
-        {
-            "nvim-treesitter/nvim-treesitter",
-            build = ":TSUpdate",
-            event = "BufReadPost",
-            config = function()
-                vim.o.foldmethod = "expr"
-                vim.o.foldexpr = "nvim_treesitter#foldexpr()"
-                vim.o.foldlevel = 20
-                require("nvim-treesitter.configs").setup(
-                    {
-                        ensure_installed = {
-                            "bash",
-                            "css",
-                            "elm",
-                            "html",
-                            "javascript",
-                            "jsdoc",
-                            "json",
-                            "lua",
-                            "regex",
-                            "ruby",
-                            "rust",
-                            "scss",
-                            "toml",
-                            "tsx",
-                            "typescript",
-                            "yaml",
-                            "vim"
-                        },
-                        sync_install = false,
-                        auto_install = false,
-                        highlight = {
-                            enable = true,
-                            use_languagetree = true,
-                            additional_vim_regex_highlighting = false
-                        },
-                        indent = {
-                            enable = true
-                        },
-                        incremental_selection = {
-                            enable = true,
-                            keymaps = {
-                                init_selection = "gnn",
-                                node_incremental = "grn",
-                                scope_incremental = "grc",
-                                node_decremental = "grm"
-                            }
-                        }
-                    }
-                )
-            end
-        },
-        {
-            "christoomey/vim-tmux-navigator",
-            keys = { "<C-h>", "<C-j>", "<C-k>", "<C-l>"}
-        },
-        {"tpope/vim-abolish", event = "BufReadPost"},
-        {"tpope/vim-commentary", event = "BufReadPost"},
-        {"tpope/vim-eunuch", cmd = { "Rename", "Remove", "Delete", "Move", "Mkdir"}},
-        {"tpope/vim-surround", event = "BufReadPost"},
     },
     {
-        performance = {
-            rtp = {
-                disabled_plugins = {
-                    "2html_plugin",
-                    "getscript",
-                    "getscriptPlugin",
-                    "gzip",
-                    "logipat",
-                    "matchit",
-                    "matchparen",
-                    "rplugin",
-                    "rrhelper",
-                    "tarPlugin",
-                    "tohtml",
-                    "tutor",
-                    "vimball",
-                    "vimballPlugin",
-                    "zip",
-                    "zipPlugin",
-                }
-            }
-        }
-    }
-)
+        "ellisonleao/gruvbox.nvim",
+        init = function()
+            vim.cmd("colorscheme gruvbox")
+        end,
+        config = {
+            contrast = "hard",
+        },
+    },
+    {
+        "neoclide/coc.nvim",
+        build = ":CocUpdate",
+        branch = "release",
+        event = "BufReadPost",
+        config = function()
+            require("coc")
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        event = "BufReadPost",
+        config = function()
+            vim.o.foldmethod = "expr"
+            vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+            vim.o.foldlevel = 20
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "bash",
+                    "css",
+                    "elm",
+                    "html",
+                    "javascript",
+                    "jsdoc",
+                    "json",
+                    "lua",
+                    "regex",
+                    "ruby",
+                    "rust",
+                    "scss",
+                    "toml",
+                    "tsx",
+                    "typescript",
+                    "yaml",
+                    "vim",
+                },
+                sync_install = false,
+                auto_install = false,
+                highlight = {
+                    enable = true,
+                    use_languagetree = true,
+                    additional_vim_regex_highlighting = false,
+                },
+                indent = {
+                    enable = true,
+                },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "gnn",
+                        node_incremental = "grn",
+                        scope_incremental = "grc",
+                        node_decremental = "grm",
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "christoomey/vim-tmux-navigator",
+        keys = { "<C-h>", "<C-j>", "<C-k>", "<C-l>" },
+    },
+    { "tpope/vim-abolish", event = "BufReadPost" },
+    { "tpope/vim-commentary", event = "BufReadPost" },
+    { "tpope/vim-eunuch", cmd = { "Rename", "Remove", "Delete", "Move", "Mkdir" } },
+    { "tpope/vim-surround", event = "BufReadPost" },
+}, {
+    performance = {
+        rtp = {
+            disabled_plugins = {
+                "2html_plugin",
+                "getscript",
+                "getscriptPlugin",
+                "gzip",
+                "logipat",
+                "matchit",
+                "matchparen",
+                "rplugin",
+                "rrhelper",
+                "tarPlugin",
+                "tohtml",
+                "tutor",
+                "vimball",
+                "vimballPlugin",
+                "zip",
+                "zipPlugin",
+            },
+        },
+    },
+})
