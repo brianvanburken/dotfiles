e8c1994104c90a735e06b41c1f468303a0188a1c
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-01-03 17:22:40 +0100

    feat: Disable perl and ruby providers

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,6 @@
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 vim.o.autoread = true -- Automatically reload files changed outside of vim
 vim.o.background = "dark"
 vim.o.backup = false
