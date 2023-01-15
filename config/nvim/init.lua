0603426c643eb216647d34ccc153402ba439bb15
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-01-15 10:40:07 +0100

    feat: add vim-hardtime to have a hard time

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -45,6 +45,16 @@ vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
 
 require("lazy").setup({
     {
+        "takac/vim-hardtime",
+        init = function()
+            vim.g.hardtime_default_on = 1
+            vim.g.hardtime_showmsg = 1
+            vim.g.hardtime_timeout = 1000
+            vim.g.hardtime_maxcount = 1
+            vim.g.hardtime_motion_with_count_resets = 1
+        end,
+    },
+    {
         "nvim-telescope/telescope.nvim",
         dependencies = { "nvim-lua/plenary.nvim", "fannheyward/telescope-coc.nvim" },
         keys = {
