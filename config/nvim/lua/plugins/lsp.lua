2f4c65f43d3a72c0034451e4b75390878862f204
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-02-01 14:20:03 +0100

    refactor: improve setup LSP servers in NeoVim

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -24,44 +24,23 @@ return {
             },
         })
 
-        vim.api.nvim_create_autocmd("LspAttach", {
-            desc = "LSP actions",
-            callback = function(args)
-                local opts = { buffer = args.buf }
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                vim.keymap.set("n", "[g", vim.diagnostic.goto_prev, opts)
-                vim.keymap.set("n", "]g", vim.diagnostic.goto_next, opts)
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                vim.keymap.set("n", "gh", vim.lsp.buf.hover, opts)
-                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-                vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
-                vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
-                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        local function lsp_mapping(buf)
+            local opts = { buffer = buf }
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+            vim.keymap.set("n", "[g", vim.diagnostic.goto_prev, opts)
+            vim.keymap.set("n", "]g", vim.diagnostic.goto_next, opts)
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+            vim.keymap.set("n", "gh", vim.lsp.buf.hover, opts)
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+            vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+            vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
+            vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 
-                vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
-                vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, opts)
-                vim.keymap.set("x", "<leader>la", vim.lsp.buf.range_code_action, opts)
-            end,
-        })
-
-        --         local function lsp_mapping(buf)
-        --             local opts = { buffer = buf }
-        --             vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        --             vim.keymap.set("n", "[g", vim.diagnostic.goto_prev, opts)
-        --             vim.keymap.set("n", "]g", vim.diagnostic.goto_next, opts)
-        --             vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        --             vim.keymap.set("n", "gh", vim.lsp.buf.hover, opts)
-        --             vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-        --             vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-        --             vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
-        --             vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
-        --             vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-
-        --             vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
-        --             vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, opts)
-        --             vim.keymap.set("x", "<leader>la", vim.lsp.buf.range_code_action, opts)
-        --         end
+            vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
+            vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, opts)
+            vim.keymap.set("x", "<leader>la", vim.lsp.buf.range_code_action, opts)
+        end
 
         -- https://github.com/numToStr/dotfiles/blob/master/neovim/.config/nvim/init.lua
         local lsp = require("lspconfig")
@@ -89,11 +68,11 @@ return {
             client.server_capabilities.semanticTokensProvider = nil
 
             ---LSP Mappings
-            -- lsp_mapping(buf)
+            lsp_mapping(buf)
         end
 
         -- Disable LSP logging
-        vim.lsp.set_log_level(vim.lsp.log_levels.OFF)
+        -- vim.lsp.set_log_level(vim.lsp.log_levels.OFF)
 
         -- Configuring native diagnostics
         vim.diagnostic.config({
@@ -105,72 +84,83 @@ return {
             },
         })
 
-        -- Lua
-        lsp.sumneko_lua.setup({
-            flags = flags,
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = {
-                Lua = {
-                    completion = {
-                        enable = true,
-                        showWord = "Disable",
-                        -- keywordSnippet = 'Disable',
-                    },
-                    runtime = {
-                        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                        version = "LuaJIT",
-                    },
-                    diagnostics = {
-                        globals = { "vim" },
-                    },
-                    workspace = {
-                        -- Make the server aware of Neovim runtime files
-                        library = { os.getenv("VIMRUNTIME") },
-                    },
-                    -- Do not send telemetry data containing a randomized but unique identifier
-                    telemetry = {
-                        enable = false,
-                    },
-                },
-            },
-        })
-
-        -- Rust
-        lsp.rust_analyzer.setup({
-            flags = flags,
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = {
-                ["rust-analyzer"] = {
-                    cargo = {
-                        allFeatures = true,
-                    },
-                    checkOnSave = {
-                        allFeatures = true,
-                        command = "clippy",
-                    },
-                },
-            },
-        })
-
-        ---List of the LSP server that don't need special configuration
-        local servers = {
-            "tsserver", -- Typescript
-            "html", -- HTML
-            "cssls", -- CSS
-            "jsonls", -- Json
-            "yamlls", -- YAML
-        }
-
         local conf = {
             flags = flags,
             capabilities = capabilities,
             on_attach = on_attach,
         }
 
-        for _, server in ipairs(servers) do
-            lsp[server].setup(conf)
+        local servers = {
+            rust_analyzer = {
+                flags = flags,
+                capabilities = capabilities,
+                on_attach = on_attach,
+                settings = {
+                    ["rust-analyzer"] = {
+                        cargo = {
+                            allFeatures = true,
+                        },
+                        checkOnSave = {
+                            allFeatures = true,
+                            command = "clippy",
+                        },
+                    },
+                },
+            },
+
+            sumneko_lua = {
+                flags = flags,
+                capabilities = capabilities,
+                on_attach = on_attach,
+                settings = {
+                    Lua = {
+                        completion = {
+                            enable = true,
+                            showWord = "Disable",
+                        },
+                        runtime = {
+                            -- LuaJIT in the case of Neovim
+                            version = "LuaJIT",
+                        },
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                        workspace = {
+                            -- Make the server aware of Neovim runtime files
+                            library = { os.getenv("VIMRUNTIME") },
+                        },
+                        -- Do not send telemetry data containing a randomized but unique identifier
+                        telemetry = {
+                            enable = false,
+                        },
+                    },
+                },
+            },
+
+            tsserver = {
+                flags = flags,
+                capabilities = capabilities,
+                on_attach = on_attach,
+                filetypes = {
+                    "javascript",
+                    "javascriptreact",
+                    "javascript.jsx",
+                    "typescript",
+                    "typescriptreact",
+                    "typescript.tsx",
+                },
+            },
+
+            taplo = conf, -- TOML
+            cssls = conf,
+            elmls = conf,
+            html = conf,
+            jsonls = conf,
+            yamlls = conf,
+        }
+
+        for server, configuration in pairs(servers) do
+            lsp[server].setup(configuration)
         end
 
         local nls = require("null-ls")
@@ -188,17 +178,20 @@ return {
                 }),
                 -- NOTE:
                 -- 1. both needs to be enabled to so prettier can apply eslint fixes
-                -- 2. prettierd should come first to prevent occassional race condition
+                -- 2. prettierd should come first to prevent occasional race condition
                 fmt.prettierd,
                 fmt.eslint_d,
 
                 fmt.rustfmt,
+                fmt.elm_format,
                 fmt.stylua,
                 fmt.shfmt.with({
                     extra_args = { "-i", 4, "-ci", "-sr" },
                 }),
 
                 -- DIAGNOSTICS --
+                dgn.codespell,
+                dgn.commitlint,
                 dgn.eslint_d,
                 dgn.shellcheck,
                 dgn.luacheck.with({
@@ -224,7 +217,7 @@ return {
                     })
                 end
 
-                -- lsp_mapping(buf)
+                lsp_mapping(buf)
             end,
         })
     end,
