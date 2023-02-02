7a680ffe3948ff2369e13adf7560cdc601d707cd
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-02-02 17:19:35 +0100

    perf: only load splitjoin.vim on keys

--- config/nvim/lua/plugins/other.lua
+++ config/nvim/lua/plugins/other.lua
@@ -3,7 +3,7 @@ return {
         "christoomey/vim-tmux-navigator",
         keys = { "<C-h>", "<C-j>", "<C-k>", "<C-l>" },
     },
-    { "AndrewRadev/splitjoin.vim", event = "BufReadPost" },
+    { "AndrewRadev/splitjoin.vim", keys = { "gS", "gJ" } },
     { "tpope/vim-abolish", event = "BufReadPost" },
     { "tpope/vim-commentary", event = "BufReadPost" },
     { "tpope/vim-eunuch", cmd = { "Rename", "Remove", "Delete", "Move", "Mkdir" } },
