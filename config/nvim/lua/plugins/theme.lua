adf74774c5d47f13da6ec9d2e72b4bab8a8ae95f
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-02-07 15:41:32 +0100

    perf: delay loading theme on VeryLazy for NeoVim

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -8,6 +8,7 @@
 
 return {
     "rose-pine/neovim",
+    event = "VeryLazy",
     name = "rose-pine",
     init = function()
         vim.api.nvim_cmd({ cmd = "colorscheme", args = { "rose-pine" } }, {})
