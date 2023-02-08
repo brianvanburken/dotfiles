89dd0ffc29a70a92de36f0f89341118167fa35a5
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-02-08 15:11:27 +0100

    feat: setup fish as default on installation

--- setup/install-macos.sh
+++ setup/install-macos.sh
@@ -10,8 +10,8 @@ export XDG_DATA_HOME="${HOME}/.local/share"
 
 # Set Homebrew prefix
 case "$(uname -m)" in
-  arm64) export HOMEBREW_PREFIX="/opt/homebrew" ;;
-  *)     export HOMEBREW_PREFIX="/usr/local" ;;
+    arm64) export HOMEBREW_PREFIX="/opt/homebrew" ;;
+    *) export HOMEBREW_PREFIX="/usr/local" ;;
 esac
 
 # Force specific PATH
@@ -20,7 +20,7 @@ export PATH="${HOMEBREW_PREFIX}/bin:${HOMEBREW_PREFIX}/sbin:${PATH}"
 # Location where to store code
 export DEV_DIR="${DEV_DIR:=${HOME}/Developer}"
 
-# Location where to store configrations
+# Location where to store configurations
 export DOT_DIR="${DOT_DIR:=${DEV_DIR}/personal/dotfiles}"
 
 # Name of dotfiles repo
@@ -30,13 +30,15 @@ readonly DOT_REPO="brianvanburken/dotfiles"
 readonly SOURCE_URL="${SOURCE_URL:=https://raw.githubusercontent.com/${DOT_REPO}/master/}"
 
 # Determine macOS version
-readonly OS_VERSION="$(sw_vers -productVersion)"
+OS_VERSION="$(sw_vers -productVersion)"
+readonly OS_VERSION
 
 # Supported macOS versions
-readonly SUPPORTED_MACOS_VERSIONS=("11", "12")
+readonly SUPPORTED_MACOS_VERSIONS=("11" "12" "13")
 
 # Determine Mac model
-readonly APPLE_COMPUTER_TYPE="$(sysctl hw.model | awk '{print $2}')"
+APPLE_COMPUTER_TYPE="$(sysctl hw.model | awk '{print $2}')"
+readonly APPLE_COMPUTER_TYPE
 
 # ANSI color codes
 readonly LIGHT_GREEN='\033[1;32m'
@@ -50,44 +52,46 @@ readonly NO_COLOR='\033[0m'
 require_manual_actions=0
 
 action() {
-  echo "${WHITE}--->${NO_COLOR} $1"
+    echo "${WHITE}--->${NO_COLOR} $1"
 }
+
 ok() {
-  echo "${LIGHT_GREEN}[OK]${NO_COLOR} $1"
+    echo "${LIGHT_GREEN}[OK]${NO_COLOR} $1"
 }
+
 input() {
-  echo "${WHITE}[INPUT]${NO_COLOR} $1"
+    echo "${WHITE}[INPUT]${NO_COLOR} $1"
 }
+
 notice() {
-  echo "${LIGHT_CYAN}[NOTICE]${NO_COLOR} $1"
+    echo "${LIGHT_CYAN}[NOTICE]${NO_COLOR} $1"
 }
+
 warning() {
-  echo "${YELLOW}[WARNING]${NO_COLOR} $1"
+    echo "${YELLOW}[WARNING]${NO_COLOR} $1"
 }
+
 error() {
-  echo "${LIGHT_RED}[ERROR]${NO_COLOR} $1"
-}
-strip_colors() {
-  sed "${1}" "s,\x1B\[[0-9;]*[a-zA-Z],,g"
+    echo "${LIGHT_RED}[ERROR]${NO_COLOR} $1"
 }
 
 cached_sudo() {
-  if sudo -n true &>/dev/null; then
-    sudo -v
-  else
-    input "We will need admin permissions for the next steps. Please enter your password and press <ENTER>"
-    sudo -v
-  fi
-  if [[ -n "${@}" ]]; then
-    sudo "${@}"
-  fi
+    if sudo -n true &> /dev/null; then
+        sudo -v
+    else
+        input "We will need admin permissions for the next steps. Please enter your password and press <ENTER>"
+        sudo -v
+    fi
+    if [[ -n "${@}" ]]; then
+        sudo "${@}"
+    fi
 }
 
 cleanup_and_exit() {
-  local exitcode="${1:-0}"
-  # Drop cached sudo credentials
-  sudo -K
-  exit "${exitcode}"
+    local exitcode="${1:-0}"
+    # Drop cached sudo credentials
+    sudo -K
+    exit "${exitcode}"
 }
 
 # Make sure we always cleanup whenever this script exits, no matter how it exits
@@ -96,32 +100,34 @@ trap cleanup_and_exit EXIT
 action "Setting up macOS"
 
 if [[ "$(uname)" != "Darwin" ]]; then
-  error "No, macOS setup does not work on non macOS machines.."
-  cleanup_and_exit 1
+    error "No, macOS setup does not work on non macOS machines.."
+    cleanup_and_exit 1
 fi
 
-readonly OS_MAJOR_VERSION="$(echo "${OS_VERSION}" | cut -f 1,1 -d '.')"
+OS_MAJOR_VERSION="$(echo "${OS_VERSION}" | cut -f 1,1 -d '.')"
 # Stylewise the value we want to test should be on the left side. But checking
 # for a value in an array does not work with the value on the left side. Other
 # possibilities (looping over the array) are harder to read. Thus we decided to
 # keep the value on the right hand side.
 if [[ ! "${SUPPORTED_MACOS_VERSIONS[@]}" =~ "${OS_MAJOR_VERSION}" ]]; then
-  error "You are running an unsupported version of macOS (${OS_MAJOR_VERSION}). Upgrade first and re-run this script."
-  cleanup_and_exit 1
+    error "You are running an unsupported version of macOS (${OS_MAJOR_VERSION})."
+    error "Upgrade first and re-run this script."
+    cleanup_and_exit 1
 fi
 
 if [[ -z "${TERM_PROGRAM}" ]]; then
-  error "This command should be run from a Terminal whiled logged in to a graphical session on your Mac. Exiting..."
-  cleanup_and_exit 1
+    error "This command should be run from a Terminal whiled logged in to a graphical session on your Mac."
+    error "Exiting..."
+    cleanup_and_exit 1
 fi
 
 if [[ ! -d "$(xcode-select -p)" ]]; then
-  action "Installing command line tools (without XCode)"
-  xcode-select --install
-  ok "Installing command line tools done. Rerun this script to install everything else."
-  cleanup_and_exit
+    action "Installing command line tools (without XCode)"
+    xcode-select --install
+    ok "Installing command line tools done. Rerun this script to install everything else."
+    cleanup_and_exit
 else
-  ok "Command line tools are installed."
+    ok "Command line tools are installed."
 fi
 
 # Get sudo credentials
@@ -129,29 +135,29 @@ cached_sudo
 
 # Install Homebrew
 if [[ ! -r "${HOMEBREW_PREFIX}/bin/brew" ]]; then
-  notice " Need to install Homebrew..."
-  action "Installing Homebrew and required components. This may take some time... ☕️ "
+    notice " Need to install Homebrew..."
+    action "Installing Homebrew and required components. This may take some time... ☕️ "
 
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  brew doctor >/dev/null
-  ok "Installing Homebrew done"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    ok "Installing Homebrew done"
 else
-  ok "Homebrew binary found."
-  action "Checking if we can brew..."
-  brew doctor >/dev/null
-  ok "Ready to brew"
+    ok "Homebrew binary found."
 fi
+
+action "Checking if we can brew..."
+brew doctor > /dev/null
+ok "Ready to brew"
 
 # After each install step, let's prolong our sudo credentials since some things may take a while
 cached_sudo
 
 # Install Brew Bundle
-if ! brew tap | grep bundle >/dev/null; then
-  notice " Need to install Homebrew Bundle"
-  brew tap Homebrew/bundle >/dev/null
-  ok "Installing Homebrew Bundle done"
+if ! brew tap | grep bundle > /dev/null; then
+    notice " Need to install Homebrew Bundle"
+    brew tap Homebrew/bundle > /dev/null
+    ok "Installing Homebrew Bundle done"
 else
-  ok "Homebrew Bundle found. Proceeding..."
+    ok "Homebrew Bundle found. Proceeding..."
 fi
 
 # Reload sudo
@@ -160,142 +166,156 @@ cached_sudo
 action "Installing some very nice apps and tools..."
 cached_sudo
 readonly brew_file="setup/Brewfile"
-if [[ ! -f "${DOT_DIR}/${brew_file}" ]]; then
-  curl -fsSL "${SOURCE_URL}/${brew_file}" | brew bundle --file=- | strip_colors
+if [[ -f "${DOT_DIR}/${brew_file}" ]]; then
+    brew bundle --file="${DOT_DIR}/${brew_file}"
 else
-  brew bundle --file="${brew_file}"
+    curl -fsSL "${SOURCE_URL}${brew_file}" | brew bundle --file=-
 fi
 ok "Installing software done."
 
 # Reload sudo
 cached_sudo
 
-readonly softwareupdates="$(softwareupdate -l 2>/dev/null)"
+softwareupdates="$(softwareupdate -l 2> /dev/null)"
 if [[ "${softwareupdates}" =~ "recommended" ]]; then
-  warning "There are recommended OS updates, please install them and re-run this script."
-  require_manual_actions=1
-  echo "${softwareupdates}"
+    warning "There are recommended OS updates, please install them and re-run this script."
+    require_manual_actions=1
+    echo "${softwareupdates}"
 else
-  ok "Your system is up to date"
+    ok "Your system is up to date"
 fi
 
-if ! nvram -x -p | grep fmm-mobileme-token-FMM &>/dev/null; then
-  warning "Find My Mac is not enabled, please configure with your AppleID account"
-  require_manual_actions=1
+if ! nvram -x -p | grep fmm-mobileme-token-FMM &> /dev/null; then
+    warning "Find My Mac is not enabled, please configure with your AppleID account"
+    require_manual_actions=1
 else
-  ok "Find My Mac is enabled"
+    ok "Find My Mac is enabled"
 fi
 
-if sudo /usr/libexec/ApplicationFirewall/socketfilterfw --getglobalstate | grep disabled &>/dev/null; then
-  action "Firewall is disabled, enabling..."
-  sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
+if cached_sudo /usr/libexec/ApplicationFirewall/socketfilterfw --getglobalstate | grep disabled &> /dev/null; then
+    action "Firewall is disabled, enabling..."
+    cached_sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
 else
-  ok "Firewall is enabled"
+    ok "Firewall is enabled"
 fi
 
 # Prolong sudo
 cached_sudo
 
-if ! fdesetup isactive &>/dev/null; then
-  warning "FileVault is not enabled"
-  cached_sudo
-  action "Enabling FileVault"
-  sudo fdesetup enable -user "$(whoami)"
-  require_reboot=1
+if ! fdesetup isactive &> /dev/null; then
+    warning "FileVault is not enabled"
+    action "Enabling FileVault"
+    cached_sudo fdesetup enable -user "$(whoami)"
 else
-  ok "Filevault is enabled"
+    ok "Filevault is enabled"
 fi
+
+# Prolong sudo
+cached_sudo
 
 # Only set the items below for mobile Macs
 if [[ "${APPLE_COMPUTER_TYPE}" =~ "MacBook" ]]; then
-  powernap_status=$(pmset -g | awk '/powernap/ {print $2}')
-  if [[ ${powernap_status} -eq 1 ]]; then
-    action "Disabling Power Nap"
-    sudo pmset -a powernap 0
-  else
-    ok "Power Nap is disabled"
-  fi
-
-  filevault_destroykeysonstandby=$(pmset -g | awk '/DestroyFVKeyOnStandby/ {print $2}' || echo 1)
-  hibernatemode=$(pmset -g | awk '/hibernatemode/ {print $2}' || echo "25")
-  standbymode=$(pmset -g | awk '/standby / {print $2}' || echo "1")
-  autopoweroff=$(pmset -g | awk '/autopoweroff / {print $2}' || echo "1")
-  #standbydelay=$(pmset -g | awk '/standbydelay/ {print $2}' || echo "86400")
-  if pmset -g batt | grep Battery &>/dev/null; then
-    if [[ ${filevault_destroykeysonstandby} -eq 1 || ${hibernatemode} -eq 25 || ${standbymode} -eq 0 || ${autopoweroff} -eq 1 ]]; then
-      action "Reverting to default sleep behavior to address stability issues with Secure Deep Sleep"
-      sudo pmset -a destroyfvkeyonstandby 0
-      sudo pmset -a standby 1
-      sudo pmset -a standbydelay 10800
-      sudo pmset -a hibernatemode 3
-      sudo pmset -a autopoweroff 0
+    powernap_status=$(pmset -g | awk '/powernap/ {print $2}')
+    if [[ ${powernap_status} -eq 1 ]]; then
+        action "Disabling Power Nap"
+        cached_sudo pmset -a powernap 0
     else
-      ok "Sleep mode correctly configured"
+        ok "Power Nap is disabled"
     fi
-  else
-    warning "No battery was found. Your MacBook battery may be dead"
-    require_manual_actions=1
-  fi
+
+    filevault_destroykeysonstandby=$(pmset -g | awk '/DestroyFVKeyOnStandby/ {print $2}' || echo 1)
+    hibernatemode=$(pmset -g | awk '/hibernatemode/ {print $2}' || echo "25")
+    standbymode=$(pmset -g | awk '/standby / {print $2}' || echo "1")
+    autopoweroff=$(pmset -g | awk '/autopoweroff / {print $2}' || echo "1")
+    #standbydelay=$(pmset -g | awk '/standbydelay/ {print $2}' || echo "86400")
+    if pmset -g batt | grep Battery &> /dev/null; then
+        if [[ ${filevault_destroykeysonstandby} -eq 1 || ${hibernatemode} -eq 25 || ${standbymode} -eq 0 || ${autopoweroff} -eq 1 ]]; then
+            action "Reverting to default sleep behavior to address stability issues with Secure Deep Sleep"
+            cached_sudo pmset -a destroyfvkeyonstandby 0
+            cached_sudo pmset -a standby 1
+            cached_sudo pmset -a standbydelay 10800
+            cached_sudo pmset -a hibernatemode 3
+            cached_sudo pmset -a autopoweroff 0
+        else
+            ok "Sleep mode correctly configured"
+        fi
+    else
+        warning "No battery was found. Your MacBook battery may be dead"
+        require_manual_actions=1
+    fi
 else
-  notice "Skipping MacBook specific settings because detected model is ${APPLE_COMPUTER_TYPE:-empty}"
+    notice "Skipping MacBook specific settings because detected model is ${APPLE_COMPUTER_TYPE:-empty}"
 fi
+
+# Prolong sudo
+cached_sudo
 
 if [[ ! -d "${HOME}/.ssh" ]]; then
-  action "Creating ssh directory and setting permissions"
-  mkdir -p "${HOME}/.ssh"
-  chmod 700 "${HOME}/.ssh"
+    action "Creating ssh directory and setting permissions"
+    mkdir -p "${HOME}/.ssh"
+    chmod 700 "${HOME}/.ssh"
 elif [[ "$(stat -f "%A" "${HOME}/.ssh")" != "700" ]]; then
-  action "Setting secure permissions for ssh directory"
-  chmod 700 "${HOME}/.ssh"
-  find "${HOME}/.ssh" -type f -exec chmod 600 {} \;
-  find "${HOME}/.ssh" -type d -exec chmod 700 {} \;
+    action "Setting secure permissions for ssh directory"
+    chmod 700 "${HOME}/.ssh"
+    find "${HOME}/.ssh" -type f -exec chmod 600 {} \;
+    find "${HOME}/.ssh" -type d -exec chmod 700 {} \;
 else
-  ok "Permissions for ssh directory are secure"
+    ok "Permissions for ssh directory are secure"
 fi
+
+# Prolong sudo
+cached_sudo
 
 action "Creating special directories"
 mkdir -p "${DEV_DIR}/{personal,oss,work}"
 
 if [ ! -d "${DOT_DIR}" ]; then
-  action "Cloning configurations"
-  git clone "https://github.com/${DOT_REPO}.git" "${DOT_DIR}"
-  cd "${DOT_DIR}"
-  git remote remove origin
-  git remote add origin "git@github.com:${DOT_REPO}.git"
-  git fetch --all
-  git branch -u origin/master master
-  git config --local user.name "Brian van Burken"
-  git config --local user.email "brianvanburken@users.noreply.github.com"
-  cd -
-  ok "Created configurations directory at ${DOT_DIR}"
+    action "Cloning configurations"
+    git clone "https://github.com/${DOT_REPO}.git" "${DOT_DIR}"
+    cd "${DOT_DIR}"
+    git remote remove origin
+    git remote add origin "git@github.com:${DOT_REPO}.git"
+    git fetch --all
+    git branch -u origin/master master
+    git config --local user.name "Brian van Burken"
+    git config --local user.email "brianvanburken@users.noreply.github.com"
+    cd -
+    ok "Created configurations directory at ${DOT_DIR}"
 else
-  ok "Dotfiles already present"
+    ok "Dotfiles already present"
 fi
+
+# Prolong sudo
+cached_sudo
 
 action "Linking dotfiles in ${HOME} to ${DOT_DIR}"
 mkdir -p "${HOME}/.config"
 files=(
-  "config/alacritty"
-  "config/asdf"
-  "config/editorconfig"
-  "config/git"
-  "config/gnupg"
-  "config/npm"
-  "config/nvim"
-  "config/ripgrep"
-  "config/rsync"
-  "config/tmux"
-  "config/zsh"
+    "config/alacritty"
+    "config/asdf"
+    "config/editorconfig"
+    "config/fish"
+    "config/git"
+    "config/npm"
+    "config/nvim"
+    "config/ripgrep"
+    "config/rsync"
+    "config/tmux"
+    "config/zsh"
 )
 for x in "${files[@]}"; do
-  action "Linking ${HOME}/.${x}"
-  rm -rf "${HOME}/.${x}"
-  ln -s "${DOT_DIR}/${x}" "${HOME}/.${x}"
+    action "Linking ${HOME}/.${x}"
+    rm -rf "${HOME}/.${x}"
+    ln -s "${DOT_DIR}/${x}" "${HOME}/.${x}"
 done
 
 action "Linking ${HOME}/.zshenv"
 rm -f "${HOME}/.zshenv"
 ln -s "${DOT_DIR}/config/zsh/zshenv" "${HOME}/.zshenv"
+
+action "Linking ${HOME}/.tool-versions"
+rm -f "${HOME}/.tool-versions"
+ln -s "${DOT_DIR}/config/asdf/.tool-versions" "${HOME}/.tool-versions"
 
 action "Linking ${DEV_DIR}/.editorignore"
 rm -f "${DEV_DIR}/.editorconfig"
@@ -303,21 +323,37 @@ ln -s "${DOT_DIR}/config/editorconfig/config" "${DEV_DIR}/.editorconfig"
 
 ok "Done linking configurations"
 
+# Prolong sudo
+cached_sudo
+
+action "Setting up fish as default shell"
+readonly fish_command="$HOMEBREW_PREFIX/bin/fish"
+readonly shell_file="/etc/shells"
+if ! grep -q "$fish_command" "$shell_file"; then
+  action "Adding fish to allowed shells in $shell_file"
+  cached_sudo echo $fish_command >> "$shell_file"
+fi
+action "Changing shell to fish"
+chsh -s "$fish_command"
+ok "Fish is now the default"
+
 action "Creating local .zshrc"
 touch "${XDG_CONFIG_HOME}/zsh/.zshrc-local"
 
-if [[ -r "${HOMEBREW_PREFIX}/bin/asdf" ]]; then
-  action "Adding asdf plugins"
-  asdf plugin-add elm || true
-  asdf plugin-add nodejs || true
-  asdf plugin-add ruby || true
-  ok "asdf plugins added"
+# Prolong sudo
+cached_sudo
 
-  action "Installing asdf versions"
-  asdf install
-  ok "Installed asdf versions"
+if [[ -x "$(command -v asdf)" ]]; then
+    action "Adding asdf plugins"
+    # https://github.com/asdf-vm/asdf/issues/276#issuecomment-1189478229
+    cut -d' ' -f1 "${HOME}/.tool-versions" | xargs -I{} asdf plugin add {} || true
+    ok "asdf plugins added"
+
+    action "Installing asdf versions"
+    asdf install
+    ok "Installed asdf versions"
 else
-  warning "asdf plugin manager not found"
+    warning "asdf plugin manager not found"
 fi
 
 # Prolong sudo
@@ -464,20 +500,27 @@ defaults write com.apple.mail DisableSendAnimations -bool true
 
 # Kill affected apps
 for app in "Dock" "Finder" "Mail"; do
-  killall "${app}" >/dev/null 2>&1
+    killall "${app}" > /dev/null 2>&1
 done
+ok "Done setting up macOS preferences"
 
 # Set SUDO commands authentication with TouchID
 # Source: https://www.imore.com/how-use-sudo-your-mac-touch-id
-# TODO: check if file already contains this configuration
-sudo sed -i '' '2i\
-auth  sufficient  pam_tid.so'$'\n' /etc/pam.d/sudo
+readonly sudo_file="/etc/pam.d/sudo"
+if ! grep -q "pam_tid.so" "$sudo_file"; then
+  action "Setting up TouchID to authenticate for sudo"
+  cached_sudo sed -i '' '2iauth       sufficient     pam_tid.so'$'\n' "$sudo_file"
+  ok "TouchID is now able to authenticate for sudo"
+else
+  ok "TouchID is already setup to authenticate for sudo"
+fi
 
 ok "Done setting macOS preferences"
 
 echo "🍺 Your MacBook is configured!"
 if [[ ${require_manual_actions} -eq 1 ]]; then
-  echo "🔎 But some items require manual actions. Check the output above for more info."
+    echo "🔎 But some items require manual actions. Check the output above for more info."
 fi
 echo "🍿 Your MacBook may also need to reboot to apply the final changes."
+
 cleanup_and_exit
