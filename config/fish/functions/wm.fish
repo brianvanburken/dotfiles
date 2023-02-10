9e9ed953ec0a3b1da3fc64f07631b3c4278e83f6
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-02-10 12:17:18 +0100

    fix: newlines now working again in wm command

--- config/fish/functions/wm.fish
+++ config/fish/functions/wm.fish
@@ -1,9 +1,9 @@
-function wm
+function wm --d "Write to working memory file"
     if test -n "$WORKING_MEMORY_FILE"
       set file "$WORKING_MEMORY_FILE"
     else
       set file "$HOME/Documents/wm.txt"
     end
 
-    echo "\n$argv" >> "$file";
+    echo -e "\n* [ ] $argv" >> "$file";
 end
