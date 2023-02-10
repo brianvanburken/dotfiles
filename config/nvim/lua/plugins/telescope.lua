915b892075212c833c0d8bd43cb0585a17fb705f
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-02-10 08:06:45 +0100

    feat: add loading Telescope on command

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,6 @@
 return {
     "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
         { "<leader>ff", "<cmd>Telescope find_files<CR>" },
