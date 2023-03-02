a5293f4d7fa35ee40f8414aee44dd15a961160d1
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-03-02 08:46:39 +0100

    feat: add Telescope resume mapping

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -3,12 +3,13 @@ return {
     cmd = "Telescope",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
+        { "<leader>fc", "<cmd>Telescope commands<CR>" },
+        { "<leader>fd", "<cmd>Telescope diagnostics<CR>" },
         { "<leader>ff", "<cmd>Telescope find_files<CR>" },
         { "<leader>fg", "<cmd>Telescope live_grep<CR>" },
-        { "<leader>fc", "<cmd>Telescope commands<CR>" },
         { "<leader>fh", "<cmd>Telescope help_tags<CR>" },
-        { "<leader>fd", "<cmd>Telescope diagnostics<CR>" },
-        { "<leader>fr", "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>" },
+        { "<leader>fr", "<cmd>Telescope resume<CR>" },
+        { "<leader>fs", "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>" },
         { "<leader>ft", "<cmd>TodoTelescope<CR>" },
     },
     opts = {
