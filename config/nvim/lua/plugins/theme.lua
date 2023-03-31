5a21280b896eec7c44e661abe502640e7b4995f3
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-03-31 10:11:49 +0200

    refactor: lazy-load NeoVim colorscheme

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,17 +1,9 @@
--- return {
---     "ellisonleao/gruvbox.nvim",
---     init = function()
---         vim.api.nvim_cmd({ cmd = "colorscheme", args = { "gruvbox" } }, {})
---     end,
---     opts = { contrast = "hard" },
--- }
-
 return {
     "rose-pine/neovim",
     event = "VeryLazy",
     name = "rose-pine",
     init = function()
-        vim.api.nvim_cmd({ cmd = "colorscheme", args = { "rose-pine" } }, {})
+        vim.cmd("colorscheme rose-pine")
     end,
     opts = true,
 }
