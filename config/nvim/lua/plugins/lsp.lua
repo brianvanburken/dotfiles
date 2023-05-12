9c602bdf6a6038ee3e3ed2adc102fc4732593830
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-05-12 08:18:13 +0200

    feat: ignore global vim for luacheck

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -123,9 +123,6 @@ return {
                             -- LuaJIT in the case of Neovim
                             version = "LuaJIT",
                         },
-                        diagnostics = {
-                            globals = { "vim" },
-                        },
                         workspace = {
                             -- Make the server aware of Neovim runtime files
                             library = { os.getenv("VIMRUNTIME") },
