7637989989e611d93f35cda767c92184078a3048
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-05-22 08:02:47 +0200

    fix: prevent alacritty_font to be set to too small size

--- config/fish/functions/alacritty_font.fish
+++ config/fish/functions/alacritty_font.fish
@@ -1,3 +1,4 @@
 function alacritty_font
-    sed -i '' -E "s/size: [0-9]+/size: $argv[1]/g" $XDG_CONFIG_HOME/alacritty/alacritty.yml
+    set font_size (math max $argv[1], 10);
+    sed -i '' -E "s/size: [0-9]+/size: $font_size/g" $XDG_CONFIG_HOME/alacritty/alacritty.yml
 end
