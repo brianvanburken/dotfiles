a272a507ab08a7fd4003b34de73141c3e2b4f6c1
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-06-14 07:59:14 +0200

    feat: set default font size of 12 if not given for alacritty

--- config/fish/functions/alacritty_font.fish
+++ config/fish/functions/alacritty_font.fish
@@ -1,4 +1,6 @@
-function alacritty_font
-    set font_size (math max $argv[1], 10);
+function alacritty_font -d "Change the font size of alacritty"
+    # Have a minimum of 10 to stay readable
+    set font_size (math max $argv[1], 12);
+
     sed -i '' -E "s/size: [0-9]+/size: $font_size/g" $XDG_CONFIG_HOME/alacritty/alacritty.yml
 end
