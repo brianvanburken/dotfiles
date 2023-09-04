699df27849bf5f488401614b5a531276beeb49e3
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-09-04 15:20:03 +0200

    feat: update lsp config for rust-analyzer

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -91,18 +91,28 @@ return {
             rust_analyzer = {
                 settings = {
                     ["rust-analyzer"] = {
+                        assist = {
+                            importEnforceGranularity = true,
+                            importPrefix = "crate",
+                        },
+                        cargo = {
+                            features = "all",
+                        },
+                        check = {
+                            extraArgs = { "--target-dir", "/tmp/rust-analyzer" },
+                            features = "all",
+                            -- command = "clippy",
+                        },
                         server = {
                             extraEnv = {
                                 CARGO_TARGET_DIR = "/tmp/rust-analyzer",
                             },
                         },
-                        cargo = {
-                            allFeatures = true,
-                        },
-                        checkOnSave = {
-                            extraArgs = { "--target-dir", "/tmp/rust-analyzer" },
-                            allFeatures = true,
-                            command = "clippy",
+                        inlayHints = {
+                            lifetimeElisionHints = {
+                                enable = true,
+                                useParameterNames = true,
+                            },
                         },
                     },
                 },
