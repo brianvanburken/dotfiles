2f0071e41a3105051c057a962487b2f6759978b0
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-09-05 09:50:24 +0200

    feat: try catppucin theme

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,10 +1,21 @@
 return {
-    "rose-pine/neovim",
-    version = "~1.2.0",
-    event = "VeryLazy",
-    name = "rose-pine",
-    init = function()
-        vim.cmd("colorscheme rose-pine")
-    end,
-    opts = true,
+    -- {
+    --     "rose-pine/neovim",
+    --     version = "~1.2.0",
+    --     event = "VeryLazy",
+    --     name = "rose-pine",
+    --     init = function()
+    --         vim.cmd("colorscheme rose-pine")
+    --     end,
+    --     opts = true,
+    -- },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        init = function()
+            vim.cmd("colorscheme catppuccin")
+        end,
+        opts = true,
+    },
 }
