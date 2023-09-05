f7b1edaabad232f336e8ee88bafbca3422f3bfb0
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-09-05 09:55:16 +0200

    feat: improve fetch from pocket using xq

--- config/fish/functions/fetch_pocket_videos.fish
+++ config/fish/functions/fetch_pocket_videos.fish
@@ -1,5 +1,6 @@
 function fetch_pocket_videos -d "Download videos from Pocket" --argument user
   curl "https://getpocket.com/users/$user/feed/unread" \
-    | rg '<link>(?:.*(?:yewtu.be|youtu(?:.be|be.com))(.*))</link>' -r 'https://youtube.com$1' \
-    | xargs -I{} yt-dlp {}
+    | xq -q 'rss channel item guid' \
+    | rg 'https://(youtube.com|youtu.be|yewtu.be)' \
+    | xargs yt-dlp
 end
