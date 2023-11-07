877c91b88808777d9ac669cd4b0ec61930ce0263
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-11-07 07:56:30 +0100

    feat: add vimeo downloading from Pocket

--- config/fish/functions/fetch_pocket_videos.fish
+++ config/fish/functions/fetch_pocket_videos.fish
@@ -1,6 +1,6 @@
 function fetch_pocket_videos -d "Download videos from Pocket" --argument user
   curl "https://getpocket.com/users/$user/feed/unread" \
     | xq -q 'rss channel item guid' \
-    | rg 'https://(youtube.com|youtu.be|yewtu.be)' \
+    | rg 'https://(youtube.com|youtu.be|yewtu.be|vimeo.com)' \
     | xargs yt-dlp
 end
