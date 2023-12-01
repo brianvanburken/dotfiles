5dcc074f630e9b89ecb977331b5ee7ad91bc0909
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-12-01 07:04:07 +0100

    feat(fetch_pocket_videos): add twitch.tv

--- config/fish/functions/fetch_pocket_videos.fish
+++ config/fish/functions/fetch_pocket_videos.fish
@@ -1,6 +1,6 @@
 function fetch_pocket_videos -d "Download videos from Pocket" --argument user
   curl "https://getpocket.com/users/$user/feed/unread" \
     | xq -q 'rss channel item guid' \
-    | rg 'https://(youtube.com|youtu.be|yewtu.be|vimeo.com)' \
+    | rg 'https://(youtube.com|youtu.be|yewtu.be|vimeo.com|twitch.tv)' \
     | xargs yt-dlp
 end
