3798b81711a08f72a99f791488dea747fe82070c
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-12-22 12:31:24 +0100

    feat(nvim): disable python3 provider

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,7 @@
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python_provider = 0
+vim.g.loaded_python3_provider = 0
 vim.g.loaded_node_provider = 0
 
 vim.o.autoread = true -- Automatically reload files changed outside of vim
