6f07c713f171884cf67f2ce0c05ac043973ff446
Brian van Burken <brianvanburken@users.noreply.github.com>
2024-01-15 08:23:37 +0100

    feat: load vim-eunuch on Copy

--- config/nvim/lua/plugins/other.lua
+++ config/nvim/lua/plugins/other.lua
@@ -9,7 +9,7 @@ return {
     {
         "tpope/vim-eunuch",
         commit = "67f3dd32b4dcd1c427085f42ff5f29c7adc645c6",
-        cmd = { "Rename", "Remove", "Delete", "Move", "Mkdir" },
+        cmd = { "Rename", "Remove", "Delete", "Move", "Mkdir", "Copy" },
     },
     { "tpope/vim-surround", commit = "3d188ed2113431cf8dac77be61b842acb64433d9", event = "BufReadPost" },
 }
