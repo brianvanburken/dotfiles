11312b6831fbefbfef862c4431734f1b7bc66f48
Brian van Burken <brianvanburken@users.noreply.github.com>
2024-04-03 12:12:53 +0200

    feat: move to hardtime to start improving how I work in vim

---
+++ config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,7 @@
+return {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    commit = "860e912895176112868c97b46277f547e149f5e6",
+    event = "BufReadPost",
+    opts = {},
+}
