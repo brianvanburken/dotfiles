12fd2cfd62ecc1f2be85818cf19777e1187a3dd0
Brian van Burken <brianvanburken@users.noreply.github.com>
2024-05-13 16:12:59 +0200

    feat: add nvim-treesitter-textobjects

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,55 +1,63 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    version = "~0.9",
-    build = ":TSUpdate",
-    event = "BufReadPost",
-    config = function()
-        vim.o.foldmethod = "expr"
-        vim.o.foldexpr = "nvim_treesitter#foldexpr()"
-        vim.o.foldlevel = 20
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "bash",
-                "css",
-                "elm",
-                "fish",
-                "html",
-                "javascript",
-                "jsdoc",
-                "json",
-                "lua",
-                "markdown",
-                "markdown_inline",
-                "regex",
-                "ruby",
-                "rust",
-                "scss",
-                "sql",
-                "toml",
-                "tsx",
-                "typescript",
-                "vim",
-                "yaml",
-            },
-            sync_install = false,
-            auto_install = false,
-            highlight = {
-                enable = true,
-                use_languagetree = true,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = {
-                enable = true,
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "gnn",
-                    node_incremental = "grn",
-                    scope_incremental = "grc",
-                    node_decremental = "grm",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        version = "~0.9",
+        build = ":TSUpdate",
+        event = "BufReadPost",
+        config = function()
+            vim.o.foldmethod = "expr"
+            vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+            vim.o.foldlevel = 20
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "bash",
+                    "css",
+                    "elm",
+                    "fish",
+                    "html",
+                    "javascript",
+                    "jsdoc",
+                    "json",
+                    "lua",
+                    "markdown",
+                    "markdown_inline",
+                    "regex",
+                    "ruby",
+                    "rust",
+                    "scss",
+                    "sql",
+                    "toml",
+                    "tsx",
+                    "typescript",
+                    "vim",
+                    "yaml",
                 },
-            },
-        })
-    end,
+                sync_install = false,
+                auto_install = false,
+                highlight = {
+                    enable = true,
+                    use_languagetree = true,
+                    additional_vim_regex_highlighting = false,
+                },
+                indent = {
+                    enable = true,
+                },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "gnn",
+                        node_incremental = "grn",
+                        scope_incremental = "grc",
+                        node_decremental = "grm",
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        commit = "2aa454e4037424c678a8ff033951a5be39e54b9c",
+        dependency = { "nvim-treesitter/nvim-treesitter" },
+        event = "BufReadPost",
+    },
 }
