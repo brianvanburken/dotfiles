3f8a65ebb995229104aa5c99bc4665f76a486e75
Brian van Burken <brianvanburken@users.noreply.github.com>
2024-05-19 09:33:18 +0200

    refactor: remove vim-commentary as it is built-in with Neovim 0.10

--- config/nvim/lua/plugins/other.lua
+++ config/nvim/lua/plugins/other.lua
@@ -5,7 +5,6 @@ return {
     },
     { "AndrewRadev/splitjoin.vim", version = "~1.2.0", keys = { "gS", "gJ" }, event = "BufReadPost" },
     { "tpope/vim-abolish", commit = "cb3dcb220262777082f63972298d57ef9e9455ec", event = "BufReadPost" },
-    { "tpope/vim-commentary", commit = "e87cd90dc09c2a203e13af9704bd0ef79303d755", event = "BufReadPost" },
     {
         "tpope/vim-eunuch",
         commit = "67f3dd32b4dcd1c427085f42ff5f29c7adc645c6",
