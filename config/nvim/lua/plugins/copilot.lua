7d3ee38b3d22083559a13dbfddda8c7b6d0ee69d
Brian van Burken <brianvanburken@users.noreply.github.com>
2024-06-08 09:52:53 +0200

    feat: setup copilot in NeoVim

---
+++ config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {
+    "github/copilot.vim",
+    event = { "BufReadPre", "BufNewFile" },
+    version = "~1.34.0",
+    config = function()
+        vim.g.copilot_no_tab_map = true
+        vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+            expr = true,
+            replace_keycodes = false
+        })
+    end,
+}
