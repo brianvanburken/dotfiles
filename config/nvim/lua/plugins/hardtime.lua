0172f4dce267fe031acee05c4d11ec3e62ef71d4
Brian van Burken <brianvanburken@users.noreply.github.com>
2024-07-23 09:23:20 +0200

    feat: disable hardtime

--- config/nvim/lua/plugins/hardtime.lua
+++ config/nvim/lua/plugins/hardtime.lua
@@ -1,5 +1,6 @@
 return {
     "m4xshen/hardtime.nvim",
+    enabled = false,
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
     commit = "860e912895176112868c97b46277f547e149f5e6",
     event = "BufReadPost",
