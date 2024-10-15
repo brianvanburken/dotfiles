628354b3b1b53596f388910b7c6bf6766f79872d
Brian van Burken <brianvanburken@users.noreply.github.com>
2024-10-15 09:05:33 +0200

    feat: add fetching of videos from Pocket

--- config/fish/functions/fetch_pocket_videos.fish
+++ config/fish/functions/fetch_pocket_videos.fish
@@ -1,6 +1,6 @@
 function fetch_pocket_videos -d "Download videos from Pocket" --argument user
-  curl "https://getpocket.com/users/$user/feed/unread" \
-    | xq -q 'rss channel item guid' \
+  curl "https://pocket-rss.com/feed/$user/json" \
+    | jq '.items.[].url' --raw-output \
     | rg 'https://(youtube.com|youtu.be|yewtu.be|vimeo.com|twitch.tv)' \
     | xargs yt-dlp
 end
