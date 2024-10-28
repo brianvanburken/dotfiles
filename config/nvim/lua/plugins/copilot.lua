123e80afd2fb9f8d308f24d44be4f5ebcd1d37ab
Brian van Burken <brianvanburken@users.noreply.github.com>
2024-10-28 08:34:38 +0100

    refactor: load CoPilot after loading file

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,7 +1,7 @@
 return {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
-    event = { "InsertEnter" },
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
         require("copilot").setup({
             suggestion = {
