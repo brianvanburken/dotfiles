bf32586f9a0287279cb1295b78aed6e76926daef
Brian van Burken <brianvanburken@users.noreply.github.com>
2024-10-28 08:46:16 +0100

    feat: load todo plugin only on commands

--- config/nvim/lua/plugins/todo-comments.lua
+++ config/nvim/lua/plugins/todo-comments.lua
@@ -1,7 +1,8 @@
 return {
     "folke/todo-comments.nvim",
     version = "~1.1.0",
-    event = "BufReadPost",
+    -- event = "BufReadPost",
+    cmd = { "TodoComments", "TodoTelescope" },
     dependencies = {
         { "nvim-lua/plenary.nvim", version = "~0.1.3" },
     },
