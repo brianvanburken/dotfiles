08842e37627c3200b38fa450df8ed20b9f9b06c2
Brian van Burken <brianvanburken@users.noreply.github.com>
2024-12-02 07:40:16 +0100

    feat(fish): tweak prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,4 +1,18 @@
 function fish_prompt
+    # Display the current directory
     echo -n (basename $PWD)
+
+    # Check the exit status of the last command
+    if test $status -ne 0
+        # Set the color to bold red if there was an error
+        set_color --bold red
+    else
+        # Set the color to bold default if the last command was successful
+        set_color --bold white
+    end
+
     echo -n ' > '
+
+    # Reset the color to default
+    set_color normal
 end
