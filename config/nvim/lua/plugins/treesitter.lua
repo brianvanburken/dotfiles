11bde5f2c5cbfa2c154c46ee87d448539278bb07
Brian van Burken <brianvanburken@users.noreply.github.com>
2024-12-16 09:27:29 +0100

    feat(neovim): ensure treesitter elixir is installed

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -12,6 +12,7 @@ return {
                 ensure_installed = {
                     "bash",
                     "css",
+                    "elixir",
                     "elm",
                     "fish",
                     "html",
