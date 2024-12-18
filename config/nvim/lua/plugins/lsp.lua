ecbef43f91d91a06a7b396b9c8ce060bcf1aa965
Brian van Burken <brianvanburken@users.noreply.github.com>
2024-12-18 07:58:27 +0100

    fix(neovim): temporary workaround for lsp issues with rust_analyzer

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -206,6 +206,17 @@ return {
             },
         }
 
+        -- https://github.com/neovim/neovim/issues/30985
+        for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+            local default_diagnostic_handler = vim.lsp.handlers[method]
+            vim.lsp.handlers[method] = function(err, result, context, config)
+                if err ~= nil and err.code == -32802 then
+                    return
+                end
+                return default_diagnostic_handler(err, result, context, config)
+            end
+        end
+
         for server, conf in pairs(servers) do
             lsp[server].setup({
                 flags = flags,
