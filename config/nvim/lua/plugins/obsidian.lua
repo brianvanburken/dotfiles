288d75c1719afca5972d4866acd50707aa278b77
Brian van Burken <brianvanburken@users.noreply.github.com>
2024-12-30 11:02:38 +0100

    feat(neovim): add obsidian plugin

---
+++ config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,29 @@
+return {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    ft = "markdown",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+        disable_frontmatter = true,
+        ui = {
+            enable = false,
+        },
+        new_notes_location = "00 Inbox",
+        templates = {
+            folder = "00 Templates",
+        },
+        daily_notes = {
+            folder = "00 Inbox",
+        },
+        workspaces = {
+            {
+                name = "Personal",
+                path = "~/Library/CloudStorage/ProtonDrive-brianvanburken@proton.me-folder/",
+                overrides = {
+                    notes_subdir = "Personal Notes",
+                },
+            },
+        },
+    },
+}
