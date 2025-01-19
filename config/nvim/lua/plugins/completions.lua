29dc8e73a8a8be943984fddfd5e5a0a18aa613b0
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-01-19 13:24:41 +0100

    feat(neovim): bump deps and remove lua-snip

--- config/nvim/lua/plugins/completions.lua
+++ config/nvim/lua/plugins/completions.lua
@@ -1,9 +1,9 @@
 -- Sources: https://vonheikemen.github.io/devlog/tools/setup-nvim-lspconfig-plus-nvim-cmp/
 return {
     "hrsh7th/nvim-cmp",
-    commit = "c4e491a87eeacf0408902c32f031d802c7eafce8",
+    commit = "8c82d0bd31299dbff7f8e780f5e06d2283de9678",
     dependencies = {
-        { "saadparwaiz1/cmp_luasnip", commit = "18095520391186d634a0045dacaa346291096566" },
+        -- { "saadparwaiz1/cmp_luasnip", commit = "18095520391186d634a0045dacaa346291096566" },
         { "hrsh7th/cmp-path", commit = "91ff86cd9c29299a64f968ebb45846c485725f23" },
         { "hrsh7th/cmp-buffer", commit = "3022dbc9166796b644a841a02de8dd1cc1d311fa" },
         { "L3MON4D3/LuaSnip", version = "~2.0.0" },
@@ -11,12 +11,12 @@ return {
     event = "BufReadPost",
     config = function()
         local cmp = require("cmp")
-        local luasnip = require("luasnip")
+        -- local luasnip = require("luasnip")
         local select_opts = { behavior = cmp.SelectBehavior.Select }
 
         local aliases = {
             nvim_lsp = "lsp",
-            luasnip = "snippet",
+            -- luasnip = "snippet",
         }
 
         cmp.setup({
@@ -27,13 +27,13 @@ return {
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 ["<C-e>"] = cmp.config.disable,
                 ["<CR>"] = cmp.mapping.confirm({ select = false }),
-                ["<C-d>"] = cmp.mapping(function(fallback)
-                    if luasnip.jumpable(1) then
-                        luasnip.jump(1)
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
+                -- ["<C-d>"] = cmp.mapping(function(fallback)
+                --     if luasnip.jumpable(1) then
+                --         luasnip.jump(1)
+                --     else
+                --         fallback()
+                --     end
+                -- end, { "i", "s" }),
                 ["<C-space>"] = cmp.mapping({
                     i = cmp.mapping.complete(),
                     c = function(
@@ -70,15 +70,9 @@ return {
             }),
             sources = cmp.config.sources({
                 { name = "nvim_lsp", max_item_count = 3 },
-                { name = "luasnip", max_item_count = 3 },
                 { name = "path", max_item_count = 3 },
                 { name = "buffer", max_item_count = 3 },
             }),
-            snippet = {
-                expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
-                end,
-            },
             formatting = {
                 format = function(entry, item)
                     item.menu = string.format("[%s]", aliases[entry.source.name] or entry.source.name)
