3cbdfd1e4c6a311155c3d7db4da6647e23b89b67
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-04-11 22:12:22 +0200

    feat(nvim): check if client is set

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -278,7 +278,7 @@ return {
         vim.api.nvim_create_autocmd("LspAttach", {
             callback = function(ev)
                 local client = vim.lsp.get_client_by_id(ev.data.client_id)
-                if client:supports_method("textDocument/completion") then
+                if client and client:supports_method("textDocument/completion") then
                     vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
                 end
             end,
