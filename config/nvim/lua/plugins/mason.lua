aed0e3460f1e04bb201e65f1112c17950a5e6d4e
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-04-14 16:47:58 +0200

    feat(nvim): use mason to ensure LSPs are installed

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -2,5 +2,21 @@ return {
     "williamboman/mason.nvim",
     version = "~1.7.0",
     event = { "BufReadPre", "BufNewFile" },
-    opts = {}
+    opts = {
+        ensure_installed = {
+            "biome",
+            "css-lsp",
+            "elixir-ls",
+            "elm-language-server",
+            "html-lsp",
+            "vscode-json-language-server",
+            "lua-language-server",
+            "rust-analyzer",
+            "stylua",
+            "tailwindcss-language-server",
+            "taplo",
+            "typescript-language-server",
+            "yaml-language-server",
+        },
+    }
 }
