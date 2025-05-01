6fa93409930247fe09fcd5de58817f927b46d563
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-05-01 10:11:59 +0200

    refactor(nvim): remove unused Telescope commands

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -6,10 +6,8 @@ return {
         { "nvim-lua/plenary.nvim", version = "~0.1.3" },
     },
     keys = {
-        { "<leader>fc", "<cmd>Telescope commands<CR>" },
         { "<leader>fd", "<cmd>Telescope diagnostics<CR>" },
         { "<leader>ff", "<cmd>Telescope find_files<CR>" },
-        { "<leader>fh", "<cmd>Telescope find_files hidden=true<CR>" },
         { "<leader>fg", "<cmd>Telescope live_grep<CR>" },
         { "<leader>fr", "<cmd>Telescope resume<CR>" },
         { "<leader>fs", "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>" },
