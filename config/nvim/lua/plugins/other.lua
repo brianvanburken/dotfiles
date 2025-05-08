07043c66ce33fb233cd39aca41c0770b5d368436
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-05-08 11:29:35 +0200

    refactor(nvim): replace vim-surround with nvim-surround

--- config/nvim/lua/plugins/other.lua
+++ config/nvim/lua/plugins/other.lua
@@ -10,5 +10,10 @@ return {
         commit = "67f3dd32b4dcd1c427085f42ff5f29c7adc645c6",
         cmd = { "Rename", "Remove", "Delete", "Move", "Mkdir", "Copy" },
     },
-    { "tpope/vim-surround", commit = "3d188ed2113431cf8dac77be61b842acb64433d9", event = "BufReadPost" },
+    {
+        "kylechui/nvim-surround",
+        version = "^3.0.0",
+        event = "BufReadPost",
+        opts = {}
+    }
 }
