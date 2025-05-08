06708f8791d2afe5863424a069c76417a7bfef17
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-05-08 14:56:42 +0200

    refactor(nvim): move treesitter config to lazy syntax

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,60 +1,45 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        version = "~0.9",
         build = ":TSUpdate",
         event = "BufReadPost",
-        config = function()
-            vim.o.foldmethod = "expr"
-            vim.o.foldexpr = "nvim_treesitter#foldexpr()"
-            vim.o.foldlevel = 20
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "bash",
-                    "css",
-                    "eex",
-                    "elixir",
-                    "elm",
-                    "fish",
-                    "heex",
-                    "html",
-                    "javascript",
-                    "jsdoc",
-                    "json",
-                    "lua",
-                    "markdown",
-                    "markdown_inline",
-                    "regex",
-                    "ruby",
-                    "rust",
-                    "scss",
-                    "sql",
-                    "toml",
-                    "tsx",
-                    "typescript",
-                    "vim",
-                    "yaml",
-                },
-                sync_install = false,
-                auto_install = false,
-                highlight = {
-                    enable = true,
-                    use_languagetree = true,
-                    additional_vim_regex_highlighting = false,
-                },
-                indent = {
-                    enable = true,
-                },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = "gnn",
-                        node_incremental = "grn",
-                        scope_incremental = "grc",
-                        node_decremental = "grm",
-                    },
-                },
-            })
-        end,
+        opts = {
+            ensure_installed = {
+                "bash",
+                "css",
+                "eex",
+                "elixir",
+                "elm",
+                "fish",
+                "heex",
+                "html",
+                "javascript",
+                "jsdoc",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "regex",
+                "ruby",
+                "rust",
+                "scss",
+                "sql",
+                "toml",
+                "tsx",
+                "typescript",
+                "vim",
+                "yaml",
+            },
+            sync_install = false,
+            auto_install = false,
+            highlight = {
+                enable = true,
+                use_languagetree = true,
+                additional_vim_regex_highlighting = false,
+            },
+            indent = {
+                enable = true,
+            },
+        }
     },
 }
