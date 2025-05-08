7aa4e23f4dbe76a3583aaa11e12438dbeac95cd4
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-05-08 15:12:51 +0200

    refactor(nvim): remove barely used coerce plugin

--- config/nvim/lua/plugins/other.lua
+++ config/nvim/lua/plugins/other.lua
@@ -1,11 +1,5 @@
 return {
     {
-        "gregorias/coerce.nvim",
-        tag = 'v3.0.0',
-        event = "BufReadPost",
-        config = true,
-    },
-    {
         "tpope/vim-eunuch",
         commit = "67f3dd32b4dcd1c427085f42ff5f29c7adc645c6",
         cmd = { "Rename", "Remove", "Delete", "Move", "Mkdir", "Copy" },
