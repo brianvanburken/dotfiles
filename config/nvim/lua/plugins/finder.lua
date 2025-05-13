3c56d734eb334c8c30d2d3e970983409f68645c4
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-05-13 09:11:35 +0200

    refactor(nvim): revert to Telescope from fzf-lua
    I don't like how it opens the picker where I have it stuck on my screen
    if I lose focus on the window and the fuzzy finding doesn't work on both
    path AND file.

--- config/nvim/lua/plugins/finder.lua
+++ config/nvim/lua/plugins/finder.lua
@@ -1,60 +1,47 @@
 return {
-    "ibhagwan/fzf-lua",
-    cmd = "FzfLua",
-    keys = {
-        { "<leader>ff", "<cmd>FzfLua files<CR>" },
-        { "<leader>fg", "<cmd>FzfLua live_grep_native<CR>" },
-        { "<leader>fr", "<cmd>FzfLua resume<CR>" },
-        { "<leader>fd", "<cmd>FzfLua diagnostics_document<CR>" },
-        { "<leader>fs", "<cmd>FzfLua lsp_document_symbols<CR>" },
-        { "<leader>fw", "<cmd>FzfLua lsp_workspace_symbols<CR>" },
-        { "<leader>ft", "<cmd>FzfLua spell_suggest<CR>" },
-    },
-    opts = {
-        "border-fused",
-        fzf_colors = {
-            true,
-            ["pointer"] = { "fg", "Normal" },
-            ["prompt"] = { "fg", "Normal" },
+    {
+        "nvim-telescope/telescope.nvim",
+        cmd = "Telescope",
+        dependencies = {
+            { "nvim-lua/plenary.nvim" },
         },
-        fzf_opts = {
-            ['--no-scrollbar'] = true,
+        keys = {
+            { "<leader>fd", "<cmd>Telescope diagnostics<CR>" },
+            { "<leader>ff", "<cmd>Telescope find_files<CR>" },
+            { "<leader>fg", "<cmd>Telescope live_grep<CR>" },
+            { "<leader>fr", "<cmd>Telescope resume<CR>" },
+            { "<leader>fs", "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>" },
         },
-        fzf_args = "--bind=change:first",
-        defaults = {
-            git_icons   = false,
-            file_icons  = false,
-            color_icons = false,
-            formatter   = "path.filename_first",
-        },
-        lsp = {
-            symbols = {
-                symbol_style = false
-            }
-        },
-        files = {
-            previewer  = false,
-            cwd_prompt = false,
-        },
-        grep = {
-            rg_opts     =
-                "--column " ..
-                "--line-number " ..
-                "--no-heading " ..
-                "--color=never " ..
-                "--trim " ..
-                "--smart-case " ..
-                "--max-columns=150 " ..
-                "-e",
-            no_header   = true,
-            no_header_i = true,
-        },
-        winopts = {
-            border   = "single",
-            backdrop = 100,
-            preview  = {
-                scrollbar = "none",
-                title = false,
+        opts = {
+            pickers = {
+                find_files = {
+                    hidden = true,
+                    previewer = false,
+                    layout_strategy = "vertical",
+                },
+            },
+            defaults = {
+                border = false,
+                sorting_strategy = "ascending",
+                layout_config = {
+                    horizontal = {
+                        prompt_position = "top",
+                    },
+                    vertical = {
+                        prompt_position = "top",
+                    },
+                },
+                vimgrep_arguments = {
+                    "rg",
+                    "--color=never",
+                    "--no-heading",
+                    "--with-filename",
+                    "--line-number",
+                    "--column",
+                    "--smart-case",
+                    "--trim"
+                }
+
             }
         },
     }
