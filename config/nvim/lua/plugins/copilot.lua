2da5e4855f33efd8ab24501b64901c37f3c8bd96
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-05-13 08:17:07 +0200

    feat(nvim): change copilot completion accept key

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -10,7 +10,7 @@ return {
             hide_during_completion = true,
             debounce = 75,
             keymap = {
-                accept = "<Tab>",
+                accept = "<C-Enter>",
             },
         },
         server_opts_overrides = {},
