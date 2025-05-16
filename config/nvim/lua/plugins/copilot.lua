44eb2007e46927129c6db1d56b57c0cc5bc365f0
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-05-16 08:27:06 +0200

    refactor(nvim): call copilot by key instead of auto

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,18 +1,44 @@
 return {
     "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
+    cmd  = "Copilot",
+    keys = {
+        {
+            "<D-j>", -- Cmd+j on macOS
+            function() require("copilot.suggestion").next() end,
+            mode = "i",
+            desc = "Next copilot suggestion",
+        },
+        {
+            "<D-k>", -- Cmd+k on macOS
+            function() require("copilot.suggestion").prev() end,
+            mode = "i",
+            desc = "Prev copilot suggestion",
+        },
+    },
     opts = {
-        panel = { enabled = true },
+        panel = {
+            enabled = false
+        },
         suggestion = {
             enabled = true,
-            auto_trigger = true,
+            auto_trigger = false,
             hide_during_completion = true,
-            debounce = 75,
             keymap = {
-                accept = "<C-Enter>",
+                accept = "<D-y>",
+                next = "<D-j>",
+                prev = "<D-k>",
+                dismiss = "<C-c>",
             },
         },
         server_opts_overrides = {},
+        filetypes = {
+            javascript = true,
+            typescript = true,
+            rust = true,
+            lua = true,
+            elixir = true,
+            elm = true,
+            ["*"] = false
+        },
     },
 }
