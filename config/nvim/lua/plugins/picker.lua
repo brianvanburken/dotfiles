6aa46ecb2c679d84cc61eee3a575a32b18aa1cce
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-05-22 13:35:39 +0200

    refactor(nvim): try mini.pick

---
+++ config/nvim/lua/plugins/picker.lua
@@ -0,0 +1,26 @@
+return {
+    'echasnovski/mini.pick',
+    keys = {
+        { '<leader>ff', '<cmd>Pick files<cr>',     desc = 'Search files' },
+        { '<leader>fg', '<cmd>Pick grep_live<cr>', desc = 'Search buffers' },
+        { '<leader>fr', '<cmd>Pick resume<cr>',    desc = 'Resume previous search' },
+    },
+    opts = function()
+        local pick = require("mini.pick")
+        return {
+            source = { show = pick.default_show },
+            options = {
+                use_cache           = true,
+                content_from_bottom = true,
+            },
+            window = {
+                config = {
+                    -- use entire editor width
+                    width = vim.o.columns,
+                    -- start at left edge
+                    col = 0,
+                },
+            },
+        }
+    end,
+}
