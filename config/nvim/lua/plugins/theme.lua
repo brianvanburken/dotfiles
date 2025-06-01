285ac61bfdbb27495584da3e8c8aa38804a07080
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-06-01 12:13:07 +0200

    feat(nvim): try a more colorful theme: rose-pine

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,47 +1,15 @@
 return {
-    -- {
-    --     "rose-pine/neovim",
-    --     version = "~1.2.0",
-    --     event = "VeryLazy",
-    --     name = "rose-pine",
-    --     init = function()
-    --         vim.cmd("colorscheme rose-pine")
-    --     end,
-    --     opts = true,
-    -- },
-    -- {
-    --     "catppuccin/nvim",
-    --     name = "catppuccin",
-    --     priority = 1000,
-    --     init = function()
-    --         vim.cmd("colorscheme catppuccin")
-    --     end,
-    --     opts = true,
-    -- },
-    -- {
-    --     "jesseleite/nvim-noirbuddy",
-    --     name = "noirbuddy",
-    --     dependencies = { "tjdevries/colorbuddy.nvim", branch = "dev" },
-    --     priority = 1000,
-    --     init = function()
-    --         vim.o.background = "dark"
-    --         vim.cmd("colorscheme noirbuddy")
-    --         require("noirbuddy").setup({
-    --             colors = {
-    --                 primary = "#8A5CF5",
-    --             },
-    --         })
-    --
-    --         -- Ensure background is transparent after the theme is loaded
-    --         vim.cmd("hi Normal ctermbg=NONE guibg=NONE")
-    --     end,
-    -- },
-    {
-        "kdheepak/monochrome.nvim",
-        priority = 1000,
-        init = function()
-            vim.g.monochrome_style = "amplified"
-            vim.cmd("colorscheme monochrome")
-        end,
+    "rose-pine/neovim",
+    lazy = false,
+    priority = 1000,
+    name = "rose-pine",
+    opts = {
+        styles = {
+            bold = false,
+            italic = false,
+        },
     },
+    init = function()
+        vim.cmd.colorscheme "rose-pine"
+    end,
 }
