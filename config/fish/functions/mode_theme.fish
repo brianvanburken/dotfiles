12715babfee00bde8b33e20655ce4f910c9bd5a0
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-06-02 08:45:20 +0200

    feat: try rose-pine theme for ghostty and fish

--- config/fish/functions/mode_theme.fish
+++ config/fish/functions/mode_theme.fish
@@ -1,9 +1,9 @@
 function mode_theme
     set system_theme (defaults read -g AppleInterfaceStyle 2>/dev/null || echo "Light")
     if [ $system_theme = "Dark" ]
-        set fish_theme "Mono Smoke"
+        set fish_theme "Rosé Pine"
     else
-        set fish_theme "Mono Lace"
+        set fish_theme "Rosé Pine Dawn"
     end
     fish_config theme save "$fish_theme"
 end
