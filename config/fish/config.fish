788c59a22f7b068739a38381003de3d41313db30
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-06-20 15:30:07 +0200

    refactor(fish): instead of variable, unset function for running it once

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,16 +5,10 @@ end
 # Lazy-load mise as I don't always need it to be active
 # only on dirs that have mise files (.mise.toml/.tool-versions)
 function lazy_load_mise --on-variable PWD
-    # Exit early if already activated
-    if set -q __mise_activated
-        return
-    end
-
     # Test for .mise.toml or .tool-versions
     if test -f mise.toml; or test -f .mise.toml; or test -f .tool-versions
-        echo "Activating mise"
         mise activate fish | source
-        set -g __mise_activated 1
+        functions -e lazy_load_mise
     end
 end
 
