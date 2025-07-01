f2b5b089bc353bc5349b42312164c44f4d9cbe65
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-07-01 14:25:32 +0200

    feat(install): support macOS Sequoia and Tahoe

--- setup/install-macos.sh
+++ setup/install-macos.sh
@@ -34,7 +34,7 @@ OS_VERSION="$(sw_vers -productVersion)"
 readonly OS_VERSION
 
 # Supported macOS versions
-readonly SUPPORTED_MACOS_VERSIONS=("11" "12" "13" "14")
+readonly SUPPORTED_MACOS_VERSIONS=("11" "12" "13" "14", "15", "16")
 
 # Determine Mac model
 APPLE_COMPUTER_TYPE="$(sysctl hw.model | awk '{print $2}')"
