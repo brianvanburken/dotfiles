f4976a08dd314a2d6e5aa95079b22ec5f023f75b
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-07-08 07:50:16 +0200

    feat(nvim): add support for Nix

---
+++ config/nvim/lsp/nix.lua
@@ -0,0 +1,8 @@
+return {
+    cmd = { "nixd" },
+    filetypes = { "nix" },
+    root_markers = {
+        "flake.nix",
+    },
+    single_file_support = true,
+}
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -19,6 +19,7 @@ return {
                 "lua",
                 "markdown",
                 "markdown_inline",
+                "nix",
                 "regex",
                 "ruby",
                 "rust",
