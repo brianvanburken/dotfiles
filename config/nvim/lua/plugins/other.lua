09601f9343ed3051e652cfddb04095efbee411c6
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-07-17 12:34:50 +0200

    feat(nvim): update deps

--- config/nvim/lua/plugins/other.lua
+++ config/nvim/lua/plugins/other.lua
@@ -1,7 +1,6 @@
 return {
     {
         "tpope/vim-eunuch",
-        commit = "67f3dd32b4dcd1c427085f42ff5f29c7adc645c6",
         cmd = { "Rename", "Remove", "Delete", "Move", "Mkdir", "Copy" },
     },
     {
