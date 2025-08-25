5a12b5eb5870464a3cf94d1f36ae9844179e5f1d
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-08-25 15:37:46 +0200

    fix(nvim): set background to terminal background

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -2,6 +2,12 @@ return {
     "shatur/neovim-ayu",
     cmd = "Colorscheme",
     init = function()
+        require('ayu').setup({
+            terminal = true,
+            overrides = {
+                Normal = { bg = "None" },
+            },
+        })
         vim.cmd.colorscheme "ayu"
     end,
 }
