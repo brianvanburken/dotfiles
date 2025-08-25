ef1718246c58fdc18e0f882a3b1200f1dab27bc5
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-08-25 11:03:03 +0200

    refactor(nvim): tweak elixir-tools lsp

--- config/nvim/lua/plugins/elixir.lua
+++ config/nvim/lua/plugins/elixir.lua
@@ -13,26 +13,27 @@ return {
         ft = ft,
         config = function()
             local elixir = require("elixir")
-            local elixirls = require("elixir.elixirls")
 
             elixir.setup {
                 projectionist = {
                     enable = false
                 },
+                credo = {
+                    enable = false
+                },
                 nextls = {
-                    enable = true
+                    enable = true,
+                    spitfire = false,
+                    init_options = {
+                        experimental = {
+                            completions = {
+                                enable = true,
+                            },
+                        },
+                    },
                 },
                 elixirls = {
-                    enable = true,
-                    settings = elixirls.settings {
-                        dialyzerEnabled = false,
-                        enableTestLenses = false,
-                    },
-                    on_attach = function()
-                        vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
-                        vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
-                        vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
-                    end,
+                    enable = false,
                 },
             }
         end,
