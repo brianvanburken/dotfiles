68686d08d914ebdaa2f90c1d1afb724f9c5a7d0f
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-08-29 07:57:04 +0200

    feat(nvim): add experimental elixir LSP

---
+++ config/nvim/lsp/expert.lua
@@ -0,0 +1,13 @@
+return {
+    cmd = { 'expert' },
+    filetypes = {
+        'elixir',
+        'eelixir',
+        'heex',
+        'surface'
+    },
+    root_markers = {
+        'mix.exs',
+        '.git'
+    },
+}
