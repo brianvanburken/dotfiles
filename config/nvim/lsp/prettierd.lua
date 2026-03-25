a60db1408ef343ee1663d6bb3f6071e9e91c85f6
Brian van Burken <brianvanburken@users.noreply.github.com>
2026-03-25 21:08:18 +0100

    feat(nvim): add prettierd LSP config

---
+++ config/nvim/lsp/prettierd.lua
@@ -0,0 +1,24 @@
+return {
+    cmd = { "prettierd", "--stdio" },
+    filetypes = {
+        "css",
+        "graphql",
+        "html",
+        "javascript",
+        "javascriptreact",
+        "json",
+        "jsonc",
+        "markdown",
+        "typescript",
+        "typescriptreact",
+        "yaml",
+    },
+    root_markers = {
+        ".prettierrc",
+        ".prettierrc.json",
+        ".prettierrc.yml",
+        ".prettierrc.yaml",
+        ".prettierrc.js",
+        "prettier.config.js"
+    },
+}
