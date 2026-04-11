dc515ec4b2f23d91fb86caf62998a952556b5681
Brian van Burken <brianvanburken@users.noreply.github.com>
2026-04-11 21:58:42 +0200

    feat(nvim): switch snacks picker from smart to files

--- config/nvim/plugin/snacks.lua
+++ config/nvim/plugin/snacks.lua
@@ -25,7 +25,7 @@ vim.api.nvim_create_autocmd("VimEnter", {
             },
         })
 
-        vim.keymap.set("n", "<leader>ff", function() Snacks.picker.smart() end, { desc = "Smart Find Files" })
+        vim.keymap.set("n", "<leader>ff", function() Snacks.picker.files() end, { desc = "Find Files" })
         vim.keymap.set("n", "<leader>fg", function() Snacks.picker.grep() end, { desc = "Grep" })
         vim.keymap.set("n", "<leader>fr", function() Snacks.picker.resume() end, { desc = "Resume" })
         vim.keymap.set("n", "<leader>fs", function() Snacks.picker.lsp_symbols() end, { desc = "LSP Symbols" })
