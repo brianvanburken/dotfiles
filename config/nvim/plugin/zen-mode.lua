9d228f30d826c1e4ed10fd17b194e709f714cc77
Brian van Burken <brianvanburken@users.noreply.github.com>
2026-04-11 21:47:15 +0200

    perf(nvim): load zen-mode lazily on first keypress
    Removes VimEnter autocmd so zen-mode.nvim is never loaded in sessions
    where <leader>z is not pressed.

--- config/nvim/plugin/zen-mode.lua
+++ config/nvim/plugin/zen-mode.lua
@@ -1,7 +1,4 @@
-vim.api.nvim_create_autocmd("VimEnter", {
-    once = true,
-    callback = function()
-        vim.pack.add({ "https://github.com/folke/zen-mode.nvim" })
-        vim.keymap.set("n", "<leader>z", function() require("zen-mode").toggle() end, { desc = "Toggle Zen Mode" })
-    end,
-})
+vim.keymap.set("n", "<leader>z", function()
+    vim.pack.add({ "https://github.com/folke/zen-mode.nvim" })
+    require("zen-mode").toggle()
+end, { desc = "Toggle Zen Mode" })
