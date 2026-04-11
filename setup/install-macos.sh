679ff3038bee6fe2f9c96dd002af58ef1ee8d654
Brian van Burken <brianvanburken@users.noreply.github.com>
2026-04-11 22:21:38 +0200

    feat(setup): suppress Passwords app first-launch prompt

--- setup/install-macos.sh
+++ setup/install-macos.sh
@@ -495,6 +495,9 @@ defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 defaults write com.apple.mail DisableReplyAnimations -bool true
 defaults write com.apple.mail DisableSendAnimations -bool true
 
+# Suppress Passwords app first-launch prompt (1Password is used instead)
+defaults write com.apple.Passwords isFirstLaunch -bool false
+
 # Disable Stage Manager
 defaults write com.apple.WindowManager GloballyEnabled -bool false
 
