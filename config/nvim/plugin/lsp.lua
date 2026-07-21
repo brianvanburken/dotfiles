2207855cf66314a676aadbc50aa92f7c56708898
Brian van Burken <brianvanburken@users.noreply.github.com>
2026-07-21 16:27:11 +0200

    feat(nvim): default for definition is `grt`

--- config/nvim/plugin/lsp.lua
+++ config/nvim/plugin/lsp.lua
@@ -38,8 +38,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
             })
         end
 
-        local bufopts = { buffer = args.buf, noremap = true, silent = true }
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
         vim.keymap.set("n", "grr", vim.lsp.buf.references, { nowait = true })
 
         -- Apply first autocomplete item if omnifunc is open, else indent as normal
