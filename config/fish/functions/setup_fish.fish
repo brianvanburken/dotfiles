4bc914932ea12872290c59ef4378d1d8acf14713
Brian van Burken <brianvanburken@users.noreply.github.com>
2026-07-22 11:09:12 +0200

    feat(fish): use save to store theme

--- config/fish/functions/setup_fish.fish
+++ config/fish/functions/setup_fish.fish
@@ -94,5 +94,5 @@ function setup_fish -d "Setup variables for Fish"
     # Rebuild fish’s completion db
     fish_update_completions
 
-    fish_config theme choose ayu
+    fish_config theme save ayu
 end
