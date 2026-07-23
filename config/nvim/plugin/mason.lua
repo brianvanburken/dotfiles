fd7db4c5e94bc8dd72511bd727d0f774ebea6fa2
Brian van Burken <brianvanburken@users.noreply.github.com>
2026-07-23 13:15:54 +0200

    perf(nvim): lazy-load Mason

--- config/nvim/plugin/mason.lua
+++ config/nvim/plugin/mason.lua
@@ -1,7 +1,13 @@
-vim.api.nvim_create_autocmd("BufReadPre", {
+local mason_bin = vim.fs.joinpath(vim.fn.stdpath("data"), "mason", "bin")
+if not vim.list_contains(vim.split(vim.env.PATH, ":", { plain = true }), mason_bin) then
+    vim.env.PATH = mason_bin .. ":" .. vim.env.PATH
+end
+
+vim.api.nvim_create_autocmd("CmdUndefined", {
+    pattern = "Mason*",
     once = true,
     callback = function()
         vim.pack.add({ "https://github.com/mason-org/mason.nvim" })
-        require("mason").setup()
+        require("mason").setup({ PATH = "skip" })
     end,
 })
