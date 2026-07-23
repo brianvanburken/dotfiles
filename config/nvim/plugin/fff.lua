8cb7631ffc263c6a1405e08bd666975246ef86d0
Brian van Burken <brianvanburken@users.noreply.github.com>
2026-07-23 13:15:53 +0200

    perf(nvim): lazy-load FFF

--- config/nvim/plugin/fff.lua
+++ config/nvim/plugin/fff.lua
@@ -1,31 +1,33 @@
-vim.api.nvim_create_autocmd("VimEnter", {
-    once = true,
-    callback = function()
+vim.g.fff = {
+    lazy_sync = true,
+    prompt = "",
+    title = "Files",
+    layout = {
+        prompt_position = "top",
+        preview_position = "bottom",
+        flex = { wrap = "bottom" },
+    },
+}
+
+local plugin
+local function fff()
+    if not plugin then
         vim.pack.add({ "https://github.com/dmtrKovalenko/fff" })
+        plugin = require("fff")
+    end
+    return plugin
+end
 
-        vim.g.fff = {
-            lazy_sync = true,
-            prompt = '',
-            title = 'Files',
-            layout = {
-                prompt_position = 'top',
-                preview_position = 'bottom',
-                flex = { wrap = 'bottom' },
-            }
-        }
+vim.keymap.set("n", "<leader>ff", function() fff().find_files() end)
+vim.keymap.set("n", "<leader>fg", function() fff().live_grep() end)
+vim.keymap.set("n", "<leader>fr", function() fff().resume() end)
 
-        vim.keymap.set("n", "<leader>ff", function() require("fff").find_files() end, {})
-        vim.keymap.set("n", "<leader>fg", function() require("fff").live_grep() end, {})
-        vim.keymap.set("n", "<leader>fr", function() require("fff").resume() end, {})
-    end,
-})
-
-vim.api.nvim_create_autocmd('PackChanged', {
+vim.api.nvim_create_autocmd("PackChanged", {
     callback = function(ev)
         local name, kind = ev.data.spec.name, ev.data.kind
-        if name == 'fff.nvim' and (kind == 'install' or kind == 'update') then
-            if not ev.data.active then vim.cmd.packadd('fff.nvim') end
-            require('fff.download').download_or_build_binary()
+        if name == "fff" and (kind == "install" or kind == "update") then
+            if not ev.data.active then vim.cmd.packadd("fff") end
+            require("fff.download").download_or_build_binary()
         end
     end,
 })
