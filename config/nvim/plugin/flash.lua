1c8c5da10a78bf0caf56570a997c9f1d40899e18
Brian van Burken <brianvanburken@users.noreply.github.com>
2026-07-23 13:15:53 +0200

    perf(nvim): lazy-load Flash

--- config/nvim/plugin/flash.lua
+++ config/nvim/plugin/flash.lua
@@ -1,16 +1,14 @@
-vim.api.nvim_create_autocmd("BufReadPost", {
-    once = true,
-    callback = function()
+local plugin
+local function flash()
+    if not plugin then
         vim.pack.add({ "https://github.com/folke/flash.nvim" })
-        local ok, flash = pcall(require, "flash")
-        if not ok then return end
-        flash.setup()
+        plugin = require("flash")
+        plugin.setup()
+    end
+    return plugin
+end
 
-        vim.keymap.set({ "n", "x", "o" }, "s", function() require("flash").jump() end, { desc = "Flash" })
-        vim.keymap.set({ "n", "x", "o" }, "S", function() require("flash").treesitter() end,
-            { desc = "Flash Treesitter" })
-        vim.keymap.set("o", "r", function() require("flash").remote() end, { desc = "Remote Flash" })
-        vim.keymap.set({ "o", "x" }, "R", function() require("flash").treesitter_search() end,
-            { desc = "Treesitter Search" })
-    end,
-})
+vim.keymap.set({ "n", "x", "o" }, "s", function() flash().jump() end, { desc = "Flash" })
+vim.keymap.set({ "n", "x", "o" }, "S", function() flash().treesitter() end, { desc = "Flash Treesitter" })
+vim.keymap.set("o", "r", function() flash().remote() end, { desc = "Remote Flash" })
+vim.keymap.set({ "o", "x" }, "R", function() flash().treesitter_search() end, { desc = "Treesitter Search" })
