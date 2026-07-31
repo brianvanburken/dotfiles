9cab7a2783f82e7abc750c617d90a42de47e2463
Brian van Burken <brianvanburken@users.noreply.github.com>
2026-07-31 14:00:22 +0200

    fix(nvim): use elm-test-rs for Elm diagnostics

--- config/nvim/lsp/elmls.lua
+++ config/nvim/lsp/elmls.lua
@@ -1,5 +1,8 @@
 return {
     cmd = { "elm-language-server" },
+    init_options = {
+        elmTestPath = "elm-test-rs",
+    },
     root_markers = {
         "elm.json",
         "elm-package.json",
