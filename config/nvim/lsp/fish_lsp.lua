7838c14ab006d72cf1b7a50f0d0014f2c2b001b9
Brian van Burken <brianvanburken@users.noreply.github.com>
2026-08-15 13:36:00 +0200

    feat(nvim): add fish lsp

---
+++ config/nvim/lsp/fish_lsp.lua
@@ -0,0 +1,10 @@
+return {
+    cmd = { "fish-lsp", "start" },
+    cmd_env = { fish_lsp_show_client_popups = false },
+    filetypes = {
+        "fish",
+    },
+    root_markers = {
+        ".git",
+    },
+}
