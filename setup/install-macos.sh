76508c91e6fa919130a351f4f874452a5aa56430
Brian van Burken <brianvanburken@users.noreply.github.com>
2026-08-17 20:09:09 +0200

    fix(setup): support macOS 27 in version check

--- setup/install-macos.sh
+++ setup/install-macos.sh
@@ -34,7 +34,7 @@ OS_VERSION="$(sw_vers -productVersion)"
 readonly OS_VERSION
 
 # Supported macOS versions
-readonly SUPPORTED_MACOS_VERSIONS=("15" "26")
+readonly SUPPORTED_MACOS_VERSIONS=("15" "26" "27")
 
 # Determine Mac model
 APPLE_COMPUTER_TYPE="$(sysctl hw.model | awk '{print $2}')"
