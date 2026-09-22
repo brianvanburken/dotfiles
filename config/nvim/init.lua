445d8afdf48ea76890660c53d450180e93ee224b
Brian van Burken <brianvanburken@users.noreply.github.com>
2026-09-22 08:55:55 +0200

    feat(nvim): disable builtin plugins

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,7 +1,23 @@
 vim.loader.enable()
 
+vim.g.did_install_default_menus = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_logiPat = 1
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_nvim_zip_plugin = 1
+vim.g.loaded_remote_plugins = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_tutor_mode_plugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
 
 vim.o.clipboard = "unnamedplus"
 vim.o.confirm = true -- Prompt to save instead of erroring on :q
